e6dedc28442e685b86302960eb5c317eac24b50b
szapp <dev.szapp@gmail.com>
2021-01-09 21:25:29 +0100

    Add localization detection language depended fixes

---
+++ src/Ninja/G1CP/Content/localization.d
@@ -0,0 +1,19 @@
+/*
+ * Guess localization (0 = EN, 1 = DE, 2 = PL, 3 = RU)
+ * Source: https://github.com/szapp/Ninja/wiki/Inject-Changes#localization
+ */
+func int Ninja_G1CP_GuessLocalization() {
+    var int pan; pan = MEM_GetSymbol("MOBNAME_PAN");
+    if (pan) {
+        var zCPar_Symbol panSymb; panSymb = _^(pan);
+        var string panName; panName = MEM_ReadString(panSymb.content);
+        if (Hlp_StrCmp(panName, "Pfanne")) { // DE (Windows 1252)
+            return 1;
+        } else if (Hlp_StrCmp(panName, "Patelnia")) { // PL (Windows 1250)
+            return 2;
+        } else if (Hlp_StrCmp(panName, "Сковорода")) { // RU (Windows 1251)
+            return 3;
+        };
+    };
+    return 0; // Otherwise EN
+};
