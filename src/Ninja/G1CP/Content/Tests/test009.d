4e56b13c9bdd34099d0c9f0fe5429042bb1a4ca3
szapp <dev.szapp@gmail.com>
2021-01-21 23:12:26 +0100

    Increase compatibility of test 9

--- src/Ninja/G1CP/Content/Tests/test009.d
+++ src/Ninja/G1CP/Content/Tests/test009.d
@@ -1,14 +1,38 @@
 /*
  * #9 NPCs freeze when fleeing
  *
- * Any NPC in the area is set to the ZS_Flee state (may not work for all of them if 'other' is invalid)
+ * Any NPC in the area is set to the ZS_Flee state
  *
  * Expected behavior: The NPCs will flee from the player without freezing (might be hard to determine when the run away)
  */
 func void Ninja_G1CP_Test_009() {
-    if (Ninja_G1CP_TestsuiteAllowManual) {
-        // AI_Teleport(hero, "PSI_TEMPLE_COURT_2"); // Clears AI queue!
-        other = MEM_CpyInst(hero); // Might be dangerous!
-        AI_SetNpcsToState(hero, ZS_Flee, 4000);
+    if (!Ninja_G1CP_TestsuiteAllowManual) {
+        return;
     };
+
+    var int symbId; symbId = MEM_FindParserSymbol("ZS_Flee");
+    if (MEM_FindParserSymbol("ZS_Flee") == -1)
+    || (MEM_FindParserSymbol("ZS_Flee_Loop") == -1)
+    || (MEM_FindParserSymbol("ZS_Flee_End") == -1) {
+        Ninja_G1CP_TestsuiteErrorDetail(9, "'ZS_Flee' (or sub-functions) not found");
+        return;
+    };
+
+    // AI_Teleport(hero, "PSI_TEMPLE_COURT_2"); // Clears AI queue!
+    AI_SetNpcsToState(hero, ZS_Ninja_G1CP_Test_009_State, 4500);
+};
+
+// Wrap ZS_Flee to ensure other is valid here (AI_SetNpcsToState is supposedly buggy)
+func void ZS_Ninja_G1CP_Test_009_State() {
+    Npc_SetTarget(self, hero);
+    Npc_GetTarget(self);
+    MEM_CallByString("ZS_Flee");
+};
+func int ZS_Ninja_G1CP_Test_009_State_Loop() {
+    Npc_SetTarget(self, hero);
+    Npc_GetTarget(self);
+    MEM_CallByString("ZS_Flee_Loop");
+};
+func void ZS_Ninja_G1CP_Test_009_State_End() {
+    MEM_CallByString("ZS_Flee_End");
 };
