1ebce341ecee6bbfb510ea21477e5d179a794763
szapp <dev.szapp@gmail.com>
2021-01-21 22:34:21 +0100

    Prevent to start 'waiting' in front of doors
    Ref #2

--- src/Ninja/G1CP/Content/Fixes/Session/fix002_FixDoor.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix002_FixDoor.d
@@ -15,11 +15,10 @@ func void Ninja_G1CP_002_FixDoor_Hook() {
     // Detach the NPC from the door
     var string door; door = Npc_GetDetectedMob(self);
     if (Hlp_StrCmp(door, "DOOR")) {                                             // Is only called for doors anyway
-        if (Wld_GetMobState(self, door) == 0)                                   // Door is closed
-        && (Ninja_G1CP_GetAIVar(Hlp_GetInstanceID(self), "AIV_MOVINGMOB", 1)) { // AI-var was set to 1 or does not exist
+        if (Wld_GetMobState(self, door) == 0) {                                 // Door is closed
             AI_UseMob(self, door,  1);                                          // Just to be safe
             AI_UseMob(self, door, -1);                                          // Actual fix
-            AI_ContinueRoutine(self);                                           // Continue to walk through the door
+            Ninja_G1CP_SetAIVar(Hlp_GetInstanceID(self), "AIV_MOVINGMOB", 0);   // Prevent starting 'ZS_WaitForPassage'
         };
     };
 };
