426f2d0a57104562092370cd074cee084f1ac732
szapp <dev.szapp@gmail.com>
2021-03-04 18:16:14 +0100

    Add world functions

---
+++ src/Ninja/G1CP/Content/Misc/world.d
@@ -0,0 +1,39 @@
+/*
+ * Check if an item exists anywhere
+ */
+func int Ninja_G1CP_IsItemInstantiated(var string name) {
+    // MEM_SearchVobByName need upper case
+    name = STR_Upper(name);
+
+    // Check if physically in world (fast hash table search)
+    if (MEM_SearchVobByName(name)) {
+        return TRUE;
+    };
+
+    // Get instance ID
+    var int id; id = MEM_FindParserSymbol(name);
+
+    // Check if in inventory or container
+    var int found; found = FALSE;
+    var int list; list = MEM_World.voblist;
+    var zCListSort l;
+    while((list) && (!found));
+        l = _^(list);
+        if (Hlp_Is_oCNpc(l.data)) {
+            var C_NPC npc; npc = _^(l.data);
+            found = Npc_HasItems(npc, id);
+        } else if (Hlp_Is_oCMobContainer(l.data)) {
+            var int vobPtr; vobPtr = l.data;
+            const int oCMobContainer__IsIn = 6833040; //0x684390
+            const int call = 0;
+            if (CALL_Begin(call)) {
+                CALL_PtrParam(_@(id));
+                CALL_PutRetValTo(_@(found));
+                CALL__thiscall(_@(vobPtr), oCMobContainer__IsIn);
+                call = CALL_End();
+            };
+        };
+        list = l.next;
+    end;
+    return found;
+};
