74cf68c59d2f561ff07a78eb3b2abd7d45378268
szapp <dev.szapp@gmail.com>
2021-03-13 13:00:49 +0100

    Simplify #12 by using FindInFunc

--- src/Ninja/G1CP/Content/Fixes/Session/fix012_RangedDoubleXP.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix012_RangedDoubleXP.d
@@ -4,51 +4,42 @@
 func int G1CP_012_RangedDoubleXP() {
     var int applied; applied = FALSE;
 
-    // Find all necessary symbols
-    var int funcId; funcId = MEM_FindParserSymbol("B_DeathXP");
+    // Get necessary symbol indices
+    var int funcId;  funcId = MEM_FindParserSymbol("B_DeathXP");
     var int cond1Id; cond1Id = MEM_FindParserSymbol("ZS_Unconscious");
-    var int cond2Id; cond2Id = MEM_GetSymbol("G1CP_012_RangedDoubleXP_Condition");
     var int cond3Id; cond3Id = MEM_FindParserSymbol("AIV_WASDEFEATEDBYSC");
-    var int funcExt; funcExt = MEM_FindParserSymbol("Npc_WasInState");
-
-    // Check if all needed symbols exist
-    if (funcId != -1) && (cond1Id != -1) && (cond3Id != -1) {
-
-        // Get the byte code of the function
-        var int tokens; tokens = MEM_ArrayCreate();
-        var int params; params = MEM_ArrayCreate();
-        var int positions; positions = MEM_ArrayCreate();
-        MEMINT_TokenizeFunction(funcID, tokens, params, positions);
-        var int len; len = MEM_ArraySize(tokens);
-
-        // Iterate over the tokens
-        repeat(i, len); var int i;
-
-            // Find "ZS_Unconscious"
-            if (MEM_ArrayRead(params, i) == cond1Id)
-            && (i+2 < len) { // Prevent errors below
-
-                // Verify immediate context: Npc_WasInState(xxxx, ZS_Unconscious)
-                if (MEM_ArrayRead(tokens, i) == zPAR_TOK_PUSHINT)
-                && (MEM_ArrayRead(tokens, i+1) == zPAR_TOK_CALLEXTERN) && (MEM_ArrayRead(params, i+1) == funcExt)
-                && (MEM_ArrayRead(tokens, i+2) != zPAR_OP_UN_NOT) {
-
-                    // Overwrite "Npc_WasInState" with "G1CP_012_RangedDoubleXP_Condition"
-                    MEMINT_OverrideFunc_Ptr = MEM_ArrayRead(positions, i+1);
-                    MEMINT_OFTokPar(zPAR_TOK_CALL, MEM_ReadInt(cond2Id + zCParSymbol_content_offset));
-
-                    // That's all
-                    applied = TRUE;
-                    break;
-                };
-            };
-        end;
-
-        // Free all the arrays
-        MEM_ArrayFree(tokens);
-        MEM_ArrayFree(params);
-        MEM_ArrayFree(positions);
+    if (funcId == -1) || (cond1Id == -1) || (cond3Id == -1) {
+        return FALSE;
     };
+
+    // Byte code to search for (not using G1CP_ReplaceCallInFunc here because of extra checks below)
+    const int bytes[4] = {zPAR_TOK_PUSHINT<<24, -1, zPAR_TOK_CALLEXTERN, -1};
+    bytes[1] = cond1Id;
+    MEM_WriteInt(_@(bytes)+9, MEM_GetFuncID(Npc_WasInState));
+    var int matches; matches = G1CP_FindInFunc(funcId, _@(bytes)+3, 10);
+
+    // Get offset of the replace function
+    var int replOff; replOff = MEM_GetSymbol("G1CP_012_RangedDoubleXP_Condition");
+    replOff = MEM_ReadInt(replOff + zCParSymbol_content_offset);
+
+    // Iterate over the matches
+    repeat(i, MEM_ArraySize(matches)); var int i;
+        var int pos; pos = MEM_ArrayRead(matches, i);
+
+        // Verify immediate context: Npc_WasInState(xxxx, ZS_Unconscious) without ! in front
+        if (MEM_ReadByte(pos+10) != zPAR_OP_UN_NOT) {
+
+            // Overwrite "Npc_WasInState" with "G1CP_012_RangedDoubleXP_Condition"
+            MEMINT_OverrideFunc_Ptr = pos+5;
+            MEMINT_OFTokPar(zPAR_TOK_CALL, replOff);
+
+            // That's all
+            applied += 1;
+        };
+    end;
+
+    // Free the array
+    MEM_ArrayFree(matches);
 
     return applied;
 };
