519bda484ca2e4831ee0765035c3c14f0cdee9de
szapp <dev.szapp@gmail.com>
2021-03-17 20:53:16 +0100

    Add replace string assignment functions

--- src/Ninja/G1CP/Content/Fixes/Session/fix125_ButcherText.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix125_ButcherText.d
@@ -2,46 +2,8 @@
  * #125 The Butcher is described as two-handed sword
  */
 func int G1CP_125_ButcherText() {
-    var int applied; applied = FALSE;
-
-    // Get necessary symbol indices
     var int symbId; symbId = MEM_FindParserSymbol("ItMw_1H_Sword_Bastard_02");
-    var int itemTextSymbId; itemTextSymbId = MEM_FindParserSymbol("C_ITEM.TEXT");
     var int oneHandedSymbId; oneHandedSymbId = MEM_FindParserSymbol("NAME_OneHanded");
-    var int twoHandedSymbId; twoHandedSymbId = MEM_FindParserSymbol("NAME_TwoHanded");
-    if (symbId == -1) || (itemTextSymbId == -1) || (oneHandedSymbId == -1) || (twoHandedSymbId == -1) {
-        return FALSE;
-    };
-
-    // Read string content
-    const string NAME_TwoHanded = ""; // Must be a constant
-    NAME_TwoHanded = G1CP_GetStringVarByIndex(twoHandedSymbId, 0, "G1CP_invalid_string");
-
-    // Find "text[4] = xxx" in the instance function
-    const int bytes[3] = {zPAR_TOK_PUSH_ARRAYVAR<<24, -1, 4 + (zPAR_TOK_ASSIGNSTR<<8)};
-    bytes[1] = itemTextSymbId;
-    var int matches; matches = G1CP_FindInFunc(symbId, _@(bytes)+3, 7);
-
-    // Iterate over all matches
-    repeat(i, MEM_ArraySize(matches)); var int i;
-        var int pos; pos = MEM_ArrayRead(matches, i);
-
-        // Check context: "text[4] = NAME_TwoHanded"
-        if (MEM_ReadByte(pos-5) == zPAR_TOK_PUSHVAR) {
-            var int varId; varId = MEM_ReadInt(pos-4);
-            if (Hlp_StrCmp(G1CP_GetStringVarByIndex(varId, 0, ""), NAME_TwoHanded)) {
-
-                // Overwrite "text[4] = NAME_TwoHanded" with "text[4] = NAME_OneHanded"
-                MEMINT_OverrideFunc_Ptr = pos-5;
-                MEMINT_OFTokPar(zPAR_TOK_PUSHVAR, oneHandedSymbId);
-
-                applied += 1;
-            };
-        };
-    end;
-
-    // Free the array
-    MEM_ArrayFree(matches);
-
-    return applied;
+    var string NAME_TwoHanded; NAME_TwoHanded = G1CP_GetStringVar("NAME_TwoHanded", 0, "G1CP_invalid_string");
+    return (G1CP_ReplaceAssignStrID(symbId, "C_ITEM.TEXT", 4, NAME_TwoHanded, oneHandedSymbId) > 0);
 };
--- src/Ninja/G1CP/Content/Misc/findInFunc.d
+++ src/Ninja/G1CP/Content/Misc/findInFunc.d
@@ -125,3 +125,128 @@ func int G1CP_ReplaceCall(var int funcId, var int needleCallId, var int replaceC
     MEM_Free(matches);
     return count;
 };
+
+/*
+ * Replace any string assignments within a function with another string (by its symbol ID)
+ * The function returns the number of replacements
+ */
+func int G1CP_ReplaceAssignStrID(var int funcId, var string assignedSymb, var int ele, var string needle,
+                                       var int replaceId) {
+    // Make sure all exist
+    if (funcId < 0)    || (funcId >= currSymbolTableLength)
+    || (replaceId < 0) || (replaceId >= currSymbolTableLength) {
+        return 0;
+    };
+
+    // Confirm replacement symbol is a string constant
+    var zCPar_Symbol replaceSymb; replaceSymb = _^(MEM_GetSymbolByIndex(replaceId));
+    if ((replaceSymb.bitfield & zCPar_Symbol_bitfield_type) != zPAR_TYPE_STRING)
+    || (!((replaceSymb.bitfield & zCPar_Symbol_bitfield_flags) & zPAR_FLAG_CONST)) {
+        MEM_SendToSpy(zERR_TYPE_FAULT, "G1CP_ReplaceAssignStrID: Argument 'replaceId' has to be a string constant");
+        return 0;
+    };
+
+    // The needle cannot be an empty string
+    if (Hlp_StrCmp(needle, "")) {
+        return 0;
+    };
+
+    // Check for assignment or only pushed string
+    var int matches;
+    var int offset;
+    if (Hlp_StrCmp(assignedSymb, "")) {
+        // Only check for pushed strings
+        matches = G1CP_FindInFunc(funcId, _@(zPAR_TOK_PUSHVAR), 1);
+        offset = 1;
+    } else {
+        // Check for string assignments
+        var int destSymbId; destSymbId = MEM_FindParserSymbol(assignedSymb);
+        if (destSymbId == -1) {
+            return 0;
+        };
+        const int bytes[3] = {-1, -1, -1};
+        if (ele <= 0) {
+            bytes[0] = zPAR_TOK_PUSHVAR<<24;
+            bytes[1] = destSymbId;
+            bytes[2] = zPAR_TOK_ASSIGNSTR;
+            matches = G1CP_FindInFunc(funcId, _@(bytes)+3, 6);
+        } else {
+            bytes[0] = zPAR_TOK_PUSH_ARRAYVAR<<24;
+            bytes[1] = destSymbId;
+            bytes[2] = ele + (zPAR_TOK_ASSIGNSTR<<8);
+            matches = G1CP_FindInFunc(funcId, _@(bytes)+3, 7);
+        };
+        offset = 5;
+    };
+
+    // Iterate over all matches
+    var int count; count = 0;
+    repeat(i, MEM_ArraySize(matches)); var int i;
+        var int pos; pos = MEM_ArrayRead(matches, i);
+
+        // Check the pushed string content against "needle"
+        if (MEM_ReadByte(pos-offset) == zPAR_TOK_PUSHVAR) {
+            var int varId; varId = MEM_ReadInt(pos-offset+1);
+            if (Hlp_StrCmp(G1CP_GetStringVarByIndex(varId, 0, ""), needle)) {
+
+                // Overwrite the string assignment with the replacement string
+                MEMINT_OverrideFunc_Ptr = pos-offset;
+                MEMINT_OFTokPar(zPAR_TOK_PUSHVAR, replaceId);
+                count += 1;
+            };
+        };
+    end;
+
+    // Free the array
+    MEM_Free(matches);
+
+    return count;
+};
+
+/*
+ * Replace any string assignments within a function with another string
+ * The function returns the number of replacements
+ */
+func int G1CP_ReplaceAssignStr(var int funcId, var string assignedSymb, var int ele, var string needle,
+                                     var string replace) {
+    // Obtain the symbol index of the replacement string
+    var int calledFrom; calledFrom = MEM_GetCallerStackPos()-5+currParserStackAddress;
+    if (MEM_ReadByte(calledFrom)   != zPAR_TOK_CALL)
+    || (MEM_ReadInt(calledFrom+1)  != MEM_GetFuncOffset(G1CP_ReplaceAssignStr))
+    || (MEM_ReadByte(calledFrom-5) != zPAR_TOK_PUSHVAR) {
+        MEM_SendToSpy(zERR_TYPE_FAULT, "G1CP_ReplaceAssignStr called from invalid context");
+        return 0;
+    };
+    var int replaceId; replaceId = MEM_ReadInt(calledFrom-4);
+
+    // Pass on
+    return G1CP_ReplaceAssignStrID(funcId, assignedSymb, ele, needle, replaceId);
+};
+
+/*
+ * Replace any pushed strings within a function with another string (by its symbol ID)
+ * The function returns the number of replacements
+ */
+func int G1CP_ReplacePushStrID(var int funcId, var string needle, var int replaceId) {
+    // Pass on
+    return G1CP_ReplaceAssignStrID(funcId, "", 0, needle, replaceId);
+};
+
+/*
+ * Replace any pushed strings within a function with another string
+ * The function returns the number of replacements
+ */
+func int G1CP_ReplacePushStr(var int funcId, var string needle, var string replace) {
+    // Obtain the symbol index of the replacement string
+    var int calledFrom; calledFrom = MEM_GetCallerStackPos()-5+currParserStackAddress;
+    if (MEM_ReadByte(calledFrom)   != zPAR_TOK_CALL)
+    || (MEM_ReadInt(calledFrom+1)  != MEM_GetFuncOffset(G1CP_ReplacePushStr))
+    || (MEM_ReadByte(calledFrom-5) != zPAR_TOK_PUSHVAR) {
+        MEM_SendToSpy(zERR_TYPE_FAULT, "G1CP_ReplacePushStr called from invalid context");
+        return 0;
+    };
+    var int replaceId; replaceId = MEM_ReadInt(calledFrom-4);
+
+    // Pass on
+    return G1CP_ReplaceAssignStrID(funcId, "", 0, needle, replaceId);
+};
