5b60d83fd63d778308c8bc151c651bda8a6eeef7
AmProsius <ff@amprosius.de>
2021-03-21 21:53:41 +0100

    Changed assigned symbols to all uppercase for consistency

--- src/Ninja/G1CP/Content/Fixes/Session/fix175_EN_RiceLordKeyName.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix175_EN_RiceLordKeyName.d
@@ -3,5 +3,5 @@
  */
 func int G1CP_175_EN_RiceLordKeyName() {
     var int symbId; symbId = MEM_GetSymbolIndex("ItKey_RB_01");
-    return (G1CP_ReplaceAssignStr(symbId, "C_Item.name", 0, "Rice Lord's Bowl", "Rice Lord's Key") > 0);
+    return (G1CP_ReplaceAssignStr(symbId, "C_ITEM.NAME", 0, "Rice Lord's Bowl", "Rice Lord's Key") > 0);
 };
