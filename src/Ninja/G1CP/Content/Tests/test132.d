01713827302647ec723a5d346b9f5d8442e70af8
szapp <dev.szapp@gmail.com>
2021-04-02 11:54:11 +0200

    fix: change test output for consitency
    Refs #132

--- src/Ninja/G1CP/Content/Tests/test132.d
+++ src/Ninja/G1CP/Content/Tests/test132.d
@@ -24,7 +24,7 @@ func int G1CP_Test_132() {
         if (Hlp_StrCmp(item.description, "Die Gruft")) {
             return TRUE;
         } else {
-            var string msg; msg = "description incorrect: description = '";
+            var string msg; msg = "Description incorrect: description = '";
             msg = ConcatStrings(msg, item.description);
             msg = ConcatStrings(msg, "'");
             G1CP_TestsuiteErrorDetail(msg);
