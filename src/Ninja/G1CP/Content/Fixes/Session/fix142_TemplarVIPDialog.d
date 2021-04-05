5420672e24b28add1591da12bb24919b9ea732df
szapp <dev.szapp@gmail.com>
2021-04-05 15:30:23 +0200

    fix: improve safety of adjusting OU function calls

--- src/Ninja/G1CP/Content/Fixes/Session/fix142_TemplarVIPDialog.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix142_TemplarVIPDialog.d
@@ -3,5 +3,5 @@
  */
 func int G1CP_142_TemplarVIPDialog() {
     var int funcId; funcId = MEM_GetSymbolIndex("Info_Tpl_8_WichtigePersonen_Info");
-    return (G1CP_OUReplaceInst(funcId, "Info_Tpl_8_WichtigePersonen_15_02", self, other, other, self) > 0);
+    return (G1CP_ReplaceOUInst(funcId, "Info_Tpl_8_WichtigePersonen_15_02", self, other, other, self) > 0);
 };
--- src/Ninja/G1CP/Content/Fixes/Session/fix197_TemplarCampDialog.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix197_TemplarCampDialog.d
@@ -3,5 +3,5 @@
  */
 func int G1CP_197_TemplarCampDialog() {
     var int funcId; funcId = MEM_GetSymbolIndex("Info_Tpl_8_DasLager_Info");
-    return (G1CP_OUReplaceInst(funcId, "Info_Tpl_8_DasLager_08_02", other, self, self, other) > 0);
+    return (G1CP_ReplaceOUInst(funcId, "Info_Tpl_8_DasLager_08_02", other, self, self, other) > 0);
 };
