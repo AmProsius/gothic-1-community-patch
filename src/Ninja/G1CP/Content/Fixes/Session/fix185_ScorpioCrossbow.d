9fb14c08b21b28a5465668fd2e9643e2ccc698e4
szapp <dev.szapp@gmail.com>
2021-04-11 20:31:57 +0200

    refactor: update function calls and changelog
    Refs #185

--- src/Ninja/G1CP/Content/Fixes/Session/fix185_ScorpioCrossbow.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix185_ScorpioCrossbow.d
@@ -2,8 +2,8 @@
  * #185 Scorpio offers to teach crossbow multiple times
  */
 func int G1CP_185_ScorpioCrossbow() {
-    if (MEM_GetSymbolIndex("GRD_205_Scorpio_CROSSBOW_Condition") != -1)
-    && (MEM_GetSymbolIndex("NPC_TALENT_CROSSBOW") != -1) {
+    if (G1CP_IsFunc("GRD_205_Scorpio_CROSSBOW_Condition", "int|none"))
+    && (G1CP_IsIntConst("NPC_TALENT_CROSSBOW", 0)) {
         HookDaedalusFuncS("GRD_205_Scorpio_CROSSBOW_Condition", "G1CP_185_ScorpioCrossbow_Hook");
         return TRUE;
     } else {
@@ -18,12 +18,8 @@ func int G1CP_185_ScorpioCrossbow_Hook() {
     G1CP_ReportFuncToSpy();
 
     // Add the new condition (other conditions remain untouched)
-    var int symbPtr; symbPtr = MEM_GetSymbol("NPC_TALENT_CROSSBOW");
-    if (symbPtr) {
-        var int NPC_TALENT_CROSSBOW; NPC_TALENT_CROSSBOW = MEM_ReadInt(symbPtr + zCParSymbol_content_offset);
-        if (Npc_GetTalentSkill(hero, NPC_TALENT_CROSSBOW) > 0) {
-            return FALSE;
-        };
+    if (Npc_GetTalentSkill(hero, G1CP_GetIntConst("NPC_TALENT_CROSSBOW", 0, 0)) > 0) {
+        return FALSE;
     };
 
     // Continue with the original function
--- src/Ninja/G1CP/Content/Tests/test185.d
+++ src/Ninja/G1CP/Content/Tests/test185.d
@@ -1,7 +1,7 @@
 /*
  * #185 Scorpio offers to teach crossbow multiple times
  *
- * The hero's guild is reset and assigned to the Old Camp. The hero is given the crossbow level 1 talent and the 
+ * The hero's guild is reset and assigned to the Old Camp. The hero is given the crossbow level 1 talent and the
  * condition function of the dialog is called.
  *
  * Expected behavior: The condition function will return FALSE.
@@ -11,32 +11,31 @@ func int G1CP_Test_185() {
     var int passed; passed = TRUE;
 
     // Check if the dialog exists
-    var int funcId; funcId = MEM_GetSymbolIndex("GRD_205_Scorpio_CROSSBOW_Condition");
+    var int funcId; funcId = G1CP_GetFuncID("GRD_205_Scorpio_CROSSBOW_Condition", "int|none");
     if (funcId == -1) {
         G1CP_TestsuiteErrorDetail("Dialog condition 'GRD_205_Scorpio_CROSSBOW_Condition' not found");
         passed = FALSE;
     };
 
     // Check if the guild variable exists
-    var int guildPtr; guildPtr = MEM_GetSymbol("GIL_GRD");
-    if (!guildPtr) {
-        G1CP_TestsuiteErrorDetail("Variable 'GIL_GRD' not found");
+    if (!G1CP_IsIntConst("GIL_GRD", 0)) {
+        G1CP_TestsuiteErrorDetail("Integer constant 'GIL_GRD' not found");
         passed = FALSE;
     };
-    var int GIL_GRD; GIL_GRD = MEM_ReadInt(guildPtr + zCParSymbol_content_offset);
 
     // Check if the talent symbol exists
-    var int talentPtr; talentPtr = MEM_GetSymbol("NPC_TALENT_CROSSBOW");
-    if (!talentPtr) {
-        G1CP_TestsuiteErrorDetail("Symbol 'NPC_TALENT_CROSSBOW' not found");
+    if (!G1CP_IsIntConst("NPC_TALENT_CROSSBOW", 0)) {
+        G1CP_TestsuiteErrorDetail("Integer constant 'NPC_TALENT_CROSSBOW' not found");
         passed = FALSE;
     };
-    var int NPC_TALENT_CROSSBOW; NPC_TALENT_CROSSBOW = MEM_ReadInt(talentPtr + zCParSymbol_content_offset);
 
     // At the latest now, we need to stop if there are fails already
     if (!passed) {
         return FALSE;
     };
+
+    var int NPC_TALENT_CROSSBOW; NPC_TALENT_CROSSBOW = G1CP_GetIntConst("NPC_TALENT_CROSSBOW", 0, 0);
+    var int GIL_GRD; GIL_GRD = G1CP_GetIntConst("GIL_GRD", 0, 0);
 
     // Backup values
     var int guildBak; guildBak = hero.guild;                                      // Player guild
