e47f31018dd37af9d2da4f8fa1e2845c319c46be
AmProsius <ff@amprosius.de>
2021-04-12 11:52:13 +0200

    refactor: simplify test of #16

--- src/Ninja/G1CP/Content/Tests/test016.d
+++ src/Ninja/G1CP/Content/Tests/test016.d
@@ -7,60 +7,33 @@
  * Expected behavior: The condition functions will return FALSE.
  */
 func int G1CP_Test_016_RunDialog(var string dialogName, var string needsInfo) {
-    var int funcId;
-    var int backupValue;
-    var int backupTold;
+    // Prior checks
+    var int funcId; funcId = G1CP_Testsuite_GetDialogConditionFuncId(dialogName);
+    G1CP_Testsuite_CheckIntConst("AIV_PASSGATE", 0);
+    var C_Npc npc; npc = G1CP_Testsuite_GetNpc("Grd_212_Torwache");
+    G1CP_Testsuite_CheckPassed();
 
-    // Check status of the test
-    var int passed; passed = TRUE;
+    // Backup values
+    var int aiVarBak; aiVarBak = G1CP_NpcGetAIVar(npc, "AIV_PASSGATE", 0);         // AI variable
+    var int toldBak; toldBak = Npc_KnowsInfo(hero, MEM_GetSymbolIndex(needsInfo)); // Told status
+    var C_Npc slfBak; slfBak = MEM_CpyInst(self);                                  // self
+    var C_Npc othBak; othBak = MEM_CpyInst(other);                                 // other
 
-    // Check if dialog exists
-    funcId = MEM_GetSymbolIndex(dialogName);
-    if (funcId == -1) {
-        G1CP_TestsuiteErrorDetail(ConcatStrings(ConcatStrings("Dialog condition '", dialogName), "' not found"));
-        passed = FALSE;
-    };
-
-    // Check if NPC and AI-variable exist
-    backupValue = G1CP_NpcIDGetAIVar(MEM_GetSymbolIndex("Grd_212_Torwache"), "AIV_PASSGATE", -42);
-    if (backupValue == -42) {
-        G1CP_TestsuiteErrorDetail("NPC 'Grd_212_Torwache' or variable 'AIV_PASSGATE' not found");
-        passed = FALSE;
-    };
-
-    // At the latest now, we need to stop if there are fails already
-    if (!passed) {
-        return FALSE;
-    };
-
-    // Set the variable to TRUE
-    G1CP_NpcIDSetAIVar(MEM_GetSymbolIndex("Grd_212_Torwache"), "AIV_PASSGATE", TRUE);
-
-    // Backup told status of required dialog
-    backupTold = Npc_KnowsInfo(hero, MEM_GetSymbolIndex(needsInfo));
-    G1CP_SetInfoTold(needsInfo, TRUE);
-
-    // Backup self and other
-    var C_Npc slfBak; slfBak = MEM_CpyInst(self);
-    var C_Npc othBak; othBak = MEM_CpyInst(other);
-
-    // Set self and other
-    self  = MEM_CpyInst(hero);
-    other = MEM_CpyInst(hero);
+    // Set new values
+    G1CP_NpcSetAIVar(npc, "AIV_PASSGATE", TRUE);                                   // AI variable
+    G1CP_SetInfoTold(needsInfo, TRUE);                                             // Told status
+    self  = MEM_CpyInst(hero);                                                     // self
+    other = MEM_CpyInst(hero);                                                     // other
 
     // Call dialog condition function
     MEM_CallByID(funcId);
     var int ret; ret = MEM_PopIntResult();
 
-    // Restore self and other
-    self  = MEM_CpyInst(slfBak);
-    other = MEM_CpyInst(othBak);
-
-    // Restore told status of required dialog
-    G1CP_SetInfoTold(needsInfo, backupTold);
-
-    // Restore value
-    G1CP_NpcIDSetAIVar(MEM_GetSymbolIndex("Grd_212_Torwache"), "AIV_PASSGATE", backupValue);
+    // Restore values
+    self  = MEM_CpyInst(slfBak);                                                   // self
+    other = MEM_CpyInst(othBak);                                                   // other
+    G1CP_NpcSetAIVar(npc, "AIV_PASSGATE", aiVarBak);                               // AI variable
+    G1CP_SetInfoTold(needsInfo, toldBak);                                          // Told status
 
     // Check return value
     if (ret) {
