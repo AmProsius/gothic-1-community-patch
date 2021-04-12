519eef48bd34e0a5f70a40ee1ef6d5c12dc9a5fa
AmProsius <ff@amprosius.de>
2021-04-12 10:24:56 +0200

    refactor: make prior checks more readable in test for #15

--- src/Ninja/G1CP/Content/Tests/test015.d
+++ src/Ninja/G1CP/Content/Tests/test015.d
@@ -9,12 +9,15 @@ func int G1CP_Test_015() {
     // Define possibly missing symbols locally
     const int ATR_STRENGTH = 4;
 
+    // Define variables for specific test
+    var string dialogFuncName; dialogFuncName = "DIA_Horatio_HelpSTR_LEARN_NOW";
+
     // Prior checks
-    var int funcId; funcId = G1CP_Testsuite_GetDialogFuncId("DIA_Horatio_HelpSTR_LEARN_NOW");
+    G1CP_Testsuite_CheckDialogFunc(dialogFuncName);
     G1CP_Testsuite_CheckPassed();
 
     // Check status of the test
-    var int result; result = TRUE;
+    var int passed; passed = TRUE;
 
     // Backup values
     var int   strengthBak; strengthBak = hero.attribute[ATR_STRENGTH];
@@ -27,18 +30,18 @@ func int G1CP_Test_015() {
 
     // First pass: strength < 100
     hero.attribute[ATR_STRENGTH] = 10;
-    MEM_CallByID(funcId);
+    MEM_CallByString(dialogFuncName);
     if (hero.attribute[ATR_STRENGTH] <= 10) {
         G1CP_TestsuiteErrorDetail("Strength was not increased when below 100");
-        result = FALSE;
+        passed = FALSE;
     };
 
     // Second pass: strength > 100
     hero.attribute[ATR_STRENGTH] = 1000;
-    MEM_CallByID(funcId);
+    MEM_CallByString(dialogFuncName);
     if (hero.attribute[ATR_STRENGTH] < 1000) {
         G1CP_TestsuiteErrorDetail("Strength was decreased when above 100");
-        result = FALSE;
+        passed = FALSE;
     };
 
     // Restore self and other
@@ -53,5 +56,5 @@ func int G1CP_Test_015() {
     hero.attribute[ATR_STRENGTH] = strengthBak;
 
     // At last
-    return result;
+    return passed;
 };
