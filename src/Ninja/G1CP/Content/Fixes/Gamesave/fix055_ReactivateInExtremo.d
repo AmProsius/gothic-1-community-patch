0348cf81b728e11bdc3d79940946f9036ba2ec1b
szapp <dev.szapp@gmail.com>
2021-04-14 00:18:51 +0200

    feat: complete the reactivation of the concert
    The implementation is a proof of concept and still very ugly. One
    remaining conceptual thing is the restarting of the music on saving.
    Because the sound-VOB is disabled on reverting, the music restarts when
    saving is complete. This could possibly be adjusted.
    
    Refs #55

--- src/Ninja/G1CP/Content/Fixes/Gamesave/fix055_ReactivateInExtremo.d
+++ src/Ninja/G1CP/Content/Fixes/Gamesave/fix055_ReactivateInExtremo.d
@@ -3,7 +3,7 @@
  */
 
 /*
- * One time initialization
+ * One-time initialization
  */
 func int G1CP_055_ReactivateInExtremo_InitSession() {
     // Check essential functions
@@ -187,16 +187,63 @@ func int G1CP_055_ReactivateInExtremo_InitSession() {
         return FALSE;
     };
 
-    // Everything in order!
-    return TRUE;
+    // Finally check if the music file exists
+    // No recyclable call necessary: This function here is only called once
+    const int _vdf_fexists                      = 7713838; //0x75B42E
+    const int zFILE_VDFS__LockCriticalSection   = 4485568; //0x4471C0
+    const int zFILE_VDFS__UnlockCriticalSection = 4485600; //0x4471E0
+    CALL__cdecl(zFILE_VDFS__LockCriticalSection);
+    CALL_IntParam(1); // Only VDF_VIRTUAL
+    CALL_PutRetValTo(_@(ret));
+    CALL_cStringPtrParam("\_WORK\DATA\SOUND\SFX\CS_INEXTREMO.WAV");
+    CALL__cdecl(_vdf_fexists);
+    CALL__cdecl(zFILE_VDFS__UnlockCriticalSection);
+    var int ret;
+    return +ret;
 };
 
 
 /*
- * One time initialization in world
+ * One-time initialization in world
  */
 func int G1CP_055_ReactivateInExtremo_InitWorld() {
-    return FALSE;
+    // Use the music zones as proxy to check for the correct world
+    var int vobPtr; vobPtr = MEM_SearchVobByName("INEXTREMO_MUSIK");
+    if (!vobPtr) || (!MEM_SearchVobByName("INEXTREMO_IE")) {
+        return -1; // Try again next time
+    };
+
+    // Check the music file
+    var int soundNameAddr; soundNameAddr = vobPtr + sizeof_zCVob + 4;
+    if (MEM_ReadInt(soundNameAddr) != zString__vtbl) {
+        return FALSE;
+    }
+    if (!Hlp_StrCmp(MEM_ReadString(soundNameAddr), "CS_INEXTREMO")) {
+        return FALSE;
+    };
+
+    // Assume now, we are in the correct world. Check for each and every way point. If even only one does not exist,
+    // this fix will be marked to never be applied
+    if (!G1CP_GetWaypoint("OCR_IE_PYMONTE"))  { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_IE_FLAIL"))    { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_IE_THOMAS"))   { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_IE_UNICORN"))  { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_IE_PFEIFFER")) { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_IE_LUTTER"))   { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_IE_FLEX"))     { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_AUDIENCE_01")) { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_AUDIENCE_02")) { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_AUDIENCE_03")) { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_CROWD_01"))    { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_CROWD_02"))    { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_CROWD_03"))    { return FALSE; };
+    if (!G1CP_GetWaypoint("OCR_CROWD_04"))    { return FALSE; };
+
+    // All confirmed, let's create the story hook
+    HookDaedalusFuncS("B_Kapitelwechsel", "G1CP_055_ReactivateInExtremoHook");
+
+    // Success, all good: Never come here again
+    return TRUE;
 };
 
 
@@ -227,8 +274,103 @@ func int G1CP_055_ReactivateInExtremo() {
     };
 
     // Check now to apply the fix
+    if (G1CP_GetIntVar("Kapitel", 0, 0) != 2)  {
+        return FALSE;
+    };
 
-    return FALSE;
+    // Safety first: How could that ever happen?
+    if (G1CP_GetIntVar("InExtremoPlaying", 0, 0))  {
+        return FALSE;
+    };
+
+    // Get the symbol indices only once
+    const int npc[14] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    const int func1Id = -2;
+    const int func2Id = -1;
+    const int info1Id = -1;
+    const int info2Id = -1;
+    const int func3Id = -1;
+    if (func1Id == -2) {
+        npc[0]  = G1CP_GetNpcInstID("InExtremo_DrPymonte");
+        npc[1]  = G1CP_GetNpcInstID("InExtremo_TheFlail");
+        npc[2]  = G1CP_GetNpcInstID("InExtremo_ThomasTheForger");
+        npc[3]  = G1CP_GetNpcInstID("InExtremo_Unicorn");
+        npc[4]  = G1CP_GetNpcInstID("InExtremo_YellowPfeiffer");
+        npc[5]  = G1CP_GetNpcInstID("InExtremo_Lutter");
+        npc[6]  = G1CP_GetNpcInstID("InExtremo_Flex");
+        npc[7]  = G1CP_GetNpcInstID("IEFan1");
+        npc[8]  = G1CP_GetNpcInstID("IEFan2");
+        npc[9]  = G1CP_GetNpcInstID("IEFan3");
+        npc[10] = G1CP_GetNpcInstID("IEFan4");
+        npc[11] = G1CP_GetNpcInstID("IE_397_Announcer");
+        npc[12] = G1CP_GetNpcInstID("Charlotte");
+        npc[13] = G1CP_GetNpcInstID("Vlk_580_Grim");
+        info1Id = G1CP_GetInfoInstID("DIA_Grim_INEXTREMO");
+        info2Id = G1CP_GetInfoInstID("IE_397_Announcer_ANNOUNCE");
+        func1Id = G1CP_GetFuncID("B_InsertInExtremo", "void|none");
+        func2Id = G1CP_GetFuncID("B_InExtremoStartMusic", "void|none");
+        func3Id = G1CP_GetFuncID("Rtn_Start_580", "void|none");
+    };
+    const string wp[13] = {
+        "OCR_IE_PYMONTE",
+        "OCR_IE_FLAIL",
+        "OCR_IE_THOMAS",
+        "OCR_IE_UNICORN",
+        "OCR_IE_PFEIFFER",
+        "OCR_IE_LUTTER",
+        "OCR_IE_FLEX",
+        "OCR_CROWD_01",
+        "OCR_CROWD_02",
+        "OCR_CROWD_03",
+        "OCR_CROWD_04",
+        "OCR_AUDIENCE_02",
+        "OCR_AUDIENCE_03"
+    };
+
+    // Update Grim's routine (only if it is his default - might have been changed)
+    var C_Npc tmp;
+    if (Npc_KnowsInfo(hero, info1Id)) {
+        tmp = Hlp_GetNpc(npc[13]);
+        if (Hlp_IsValidNpc(tmp)) {
+            var int rtnId; rtnId = MEM_ReadInt(_@(tmp)+536); // oCNpc.daily_routine (read as integer)
+            if (rtnId == func3Id) {
+                Npc_ExchangeRoutine(tmp, "INEXTREMO");
+            };
+        };
+    };
+
+    // Start playing if the concert had started
+    var int i; var int id;
+    var int nmax; nmax = 11;
+    if (Npc_KnowsInfo(hero, info2Id)) {
+        MEM_CallById(func2Id);
+        nmax = 13;
+    } else {
+        // Update daily routine to announcing
+        repeat(i, 2);
+            id = MEM_ReadStatArr(npc, i+11);
+            tmp = Hlp_GetNpc(id);
+            if (!Hlp_IsValidNpc(tmp)) {
+                Wld_InsertNpc(id, MEM_ReadStatStringArr(wp, i+11));
+                tmp = Hlp_GetNpc(id);
+            };
+            Npc_ExchangeRoutine(tmp, "ANNOUNCE");
+        end;
+    };
+
+    // Insert the NPCs or reinstate their daily routine of everyone (except Grim)
+    repeat(i, nmax);
+        id = MEM_ReadStatArr(npc, i);
+        tmp = Hlp_GetNpc(id);
+        if (!Hlp_IsValidNpc(tmp)) {
+            Wld_InsertNpc(id, MEM_ReadStatStringArr(wp, i));
+            tmp = Hlp_GetNpc(id);
+        };
+        Npc_ExchangeRoutine(tmp, "CONCERT");
+    end;
+
+    // Hope that does it!
+    return TRUE;
 };
 
 
@@ -241,5 +383,99 @@ func int G1CP_055_ReactivateInExtremoRevert() {
         return FALSE;
     };
 
-    return FALSE;
+    // Get the symbol indices only once
+    const int npc[14] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    const int func1Id = -2;
+    const int func2Id = -1;
+    const int func3Id = -1;
+    if (func1Id == -2) {
+        npc[0]  = G1CP_GetNpcInstID("InExtremo_DrPymonte");
+        npc[1]  = G1CP_GetNpcInstID("InExtremo_TheFlail");
+        npc[2]  = G1CP_GetNpcInstID("InExtremo_ThomasTheForger");
+        npc[3]  = G1CP_GetNpcInstID("InExtremo_Unicorn");
+        npc[4]  = G1CP_GetNpcInstID("InExtremo_YellowPfeiffer");
+        npc[5]  = G1CP_GetNpcInstID("InExtremo_Lutter");
+        npc[6]  = G1CP_GetNpcInstID("InExtremo_Flex");
+        npc[7]  = G1CP_GetNpcInstID("IEFan1");
+        npc[8]  = G1CP_GetNpcInstID("IEFan2");
+        npc[9]  = G1CP_GetNpcInstID("IEFan3");
+        npc[10] = G1CP_GetNpcInstID("IEFan4");
+        npc[11] = G1CP_GetNpcInstID("IE_397_Announcer");
+        npc[12] = G1CP_GetNpcInstID("Charlotte");
+        npc[13] = G1CP_GetNpcInstID("Vlk_580_Grim");
+        func1Id = G1CP_GetInfoInstID("DIA_Grim_INEXTREMO");
+        func2Id = G1CP_GetFuncID("B_InExtremoStopMusic", "void|none");
+        func3Id = G1CP_GetFuncID("Rtn_InExtremo_580", "void|none");
+    };
+
+    // Create call once
+    var int npcPtr;
+    var int mobPtr;
+    const int oCMobInter__InterruptInteraction = 6812512; //0x67F360
+    const int call = 0;
+    if (!call) {
+        CALL_Open();
+        CALL_PtrParam(_@(npcPtr));
+        CALL__thiscall(_@(mobPtr), oCMobInter__InterruptInteraction);
+        call = CALL_Close();
+    };
+
+    // De-register mob users and remove all NPCs
+    var oCNpc tmp;
+    repeat(i, 13); var int i;
+        var int id; id = MEM_ReadStatArr(npc, i);
+        tmp = Hlp_GetNpc(id);
+        if (Hlp_IsValidNpc(tmp)) {
+            if (tmp.interactMob) {
+                npcPtr = _@(tmp);
+                mobPtr = tmp.interactMob;
+                ASM_Run(call);
+            };
+        };
+        Wld_RemoveNpc(id);
+        MEM_WriteInt(MEM_GetSymbolByIndex(id) + zCParSymbol_offset_offset, 0); // Reset instance
+    end;
+
+    // Turn off the music
+    MEM_CallById(func2Id);
+
+    // Cannot assume Grim's daily routine is unchanged! Only change it if it is set to "inextremo"
+    tmp = Hlp_GetNpc(npc[13]);
+    if (Hlp_IsValidNpc(tmp)) {
+        var int rtnId; rtnId = MEM_ReadInt(_@(tmp)+536); // oCNpc.daily_routine (read as integer)
+        if (rtnId == func3Id) {
+            Npc_ExchangeRoutine(tmp, "START");
+        };
+    };
+    return TRUE;
+};
+
+
+/*
+ * Hook after "B_Kapitelwechsel" to enable/disable the InExtremo concert
+ */
+func void G1CP_055_ReactivateInExtremoHook(var int num) {
+    G1CP_ReportFuncToSpy();
+
+    var int chBefore; chBefore = G1CP_GetIntVar("Kapitel", 0, 0);
+
+    // First call the original function
+    PassArgumentI(num);
+    ContinueCall();
+
+    var int chAfter; chAfter = G1CP_GetIntVar("Kapitel", 0, 0);
+
+    if (chAfter == chBefore) || (chAfter != num) {
+        // What the heck
+        return;
+    };
+
+    // Apply/revert fix (call all to update the lookup table properly)
+    var int r;
+    if (chAfter == 2) {
+        MEM_Call(G1CP_GamesaveFixes_Apply);
+    } else if (chBefore == 2) {
+        MEM_Call(G1CP_GamesaveFixes_Revert);
+        MEM_Call(G1CP_GamesaveFixes_Apply);
+    };
 };
