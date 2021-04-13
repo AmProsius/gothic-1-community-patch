8cfb08a5d54e46acf09378750fa2170e971f8da1
AmProsius <ff@amprosius.de>
2021-04-13 16:47:46 +0200

    fix: rearrange assignments of test of #16

--- src/Ninja/G1CP/Content/Tests/test016.d
+++ src/Ninja/G1CP/Content/Tests/test016.d
@@ -8,32 +8,32 @@
  */
 func int G1CP_Test_016_RunDialog(var string dialogName, var string needsInfo) {
     // Prior checks
-    var int funcId; funcId = G1CP_Testsuite_GetDialogConditionFuncId(dialogName);
+    var int   funcId; funcId = G1CP_Testsuite_GetDialogConditionFuncId(dialogName);
+    var C_Npc npc;    npc    = G1CP_Testsuite_GetNpc("Grd_212_Torwache");
     G1CP_Testsuite_CheckIntConst("AIV_PASSGATE", 0);
-    var C_Npc npc; npc = G1CP_Testsuite_GetNpc("Grd_212_Torwache");
     G1CP_Testsuite_CheckPassed();
 
     // Backup values
-    var int aiVarBak; aiVarBak = G1CP_NpcGetAIVar(npc, "AIV_PASSGATE", 0);         // AI variable
-    var int toldBak; toldBak = Npc_KnowsInfo(hero, MEM_GetSymbolIndex(needsInfo)); // Told status
-    var C_Npc slfBak; slfBak = MEM_CpyInst(self);                                  // self
-    var C_Npc othBak; othBak = MEM_CpyInst(other);                                 // other
+    var int aiVarBak; aiVarBak = G1CP_NpcGetAIVar(npc, "AIV_PASSGATE", 0);           // AI variable
+    var int toldBak;  toldBak  = Npc_KnowsInfo(hero, MEM_GetSymbolIndex(needsInfo)); // Told status
+    var C_Npc slfBak; slfBak   = MEM_CpyInst(self);                                  // self
+    var C_Npc othBak; othBak   = MEM_CpyInst(other);                                 // other
 
     // Set new values
-    G1CP_NpcSetAIVar(npc, "AIV_PASSGATE", TRUE);                                   // AI variable
-    G1CP_SetInfoTold(needsInfo, TRUE);                                             // Told status
-    self  = MEM_CpyInst(hero);                                                     // self
-    other = MEM_CpyInst(hero);                                                     // other
+    G1CP_NpcSetAIVar(npc, "AIV_PASSGATE", TRUE);                                     // AI variable
+    G1CP_SetInfoTold(needsInfo, TRUE);                                               // Told status
+    self  = MEM_CpyInst(hero);                                                       // self
+    other = MEM_CpyInst(hero);                                                       // other
 
     // Call dialog condition function
     MEM_CallByID(funcId);
     var int ret; ret = MEM_PopIntResult();
 
     // Restore values
-    self  = MEM_CpyInst(slfBak);                                                   // self
-    other = MEM_CpyInst(othBak);                                                   // other
-    G1CP_NpcSetAIVar(npc, "AIV_PASSGATE", aiVarBak);                               // AI variable
-    G1CP_SetInfoTold(needsInfo, toldBak);                                          // Told status
+    self  = MEM_CpyInst(slfBak);                                                     // self
+    other = MEM_CpyInst(othBak);                                                     // other
+    G1CP_NpcSetAIVar(npc, "AIV_PASSGATE", aiVarBak);                                 // AI variable
+    G1CP_SetInfoTold(needsInfo, toldBak);                                            // Told status
 
     // Check return value
     if (ret) {
