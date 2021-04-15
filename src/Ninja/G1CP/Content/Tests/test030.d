0649e311e341febb946bd7b1d05c860d5bc0925d
AmProsius <ff@amprosius.de>
2021-04-15 15:24:12 +0200

    refactor: simplify test of #30

--- src/Ninja/G1CP/Content/Tests/test030.d
+++ src/Ninja/G1CP/Content/Tests/test030.d
@@ -10,34 +10,10 @@ func void G1CP_Test_030() {
         return;
     };
 
-    // Check status of the test
-    var int passed; passed = TRUE;
-
-    // Check if the dialog exists
-    var int funcId; funcId = MEM_GetSymbolIndex("DIA_Silas_Trade");
-    if (funcId == -1) {
-        G1CP_TestsuiteErrorDetail("Info 'DIA_Silas_Trade' not found");
-        passed = FALSE;
-    };
-
-    // Find Silas
-    var int symbId; symbId = MEM_GetSymbolIndex("Org_841_Silas");
-    if (symbId == -1) {
-        G1CP_TestsuiteErrorDetail("NPC 'Org_841_Silas' not found");
-        return;
-    };
-
-    // Check if Silas exists in the world
-    var C_Npc silas; silas = Hlp_GetNpc(symbId);
-    if (!Hlp_IsValidNpc(silas)) {
-        G1CP_TestsuiteErrorDetail("NPC 'Org_841_Silas' not valid");
-        passed = FALSE;
-    };
-
-    // At the latest now, we need to stop if there are fails already
-    if (!passed) {
-        return;
-    };
+    // Prior checks
+    G1CP_Testsuite_CheckInfo("DIA_Silas_Trade");
+    var C_Npc npc; npc = G1CP_Testsuite_GetNpc("Org_841_Silas");
+    G1CP_Testsuite_CheckPassed();
 
     // Teleport the hero to Silas
     AI_Teleport(hero, silas.wp);
