82310c5f7c6f944df70cc40757948b499b811596
AmProsius <ff@amprosius.de>
2021-04-15 10:13:56 +0200

    refactor: simplify test of #27

--- src/Ninja/G1CP/Content/Tests/test027.d
+++ src/Ninja/G1CP/Content/Tests/test027.d
@@ -6,67 +6,34 @@
  * Expected behavior: The variable "drax_Lehrer_frei" is set/not set and log entry is created/not created accordingly.
  */
 func int G1CP_Test_027() {
+    // Define variables for specific test
+    var string bierBekommenName; bierBekommenName = "drax_bierbekommen";
+    var string lehrerFreiName;   lehrerFreiName   = "drax_Lehrer_frei";
+    var string topicName;        topicName        = "GE_TeacherOW";
+
     // Prior checks
     var int funcId; funcId = G1CP_Testsuite_GetDialogFuncId("Org_819_Drax_Scavenger_Info");
+    var int beerId; beerId = G1CP_Testsuite_GetItemId("ItFoBeer");
+    G1CP_Testsuite_CheckIntVar(bierBekommenName, 0);
+    G1CP_Testsuite_CheckIntVar(lehrerFreiName, 0);
+    G1CP_Testsuite_CheckIntConst(topicName, 0);
+    G1CP_Testsuite_CheckPassed();
 
-    // Check status of the test
-    var int passed; passed = TRUE;
-
-    // Check if dialog exists
-    var int funcId; funcId = MEM_GetSymbolIndex("Org_819_Drax_Scavenger_Info");
-    if (funcId == -1) {
-        G1CP_TestsuiteErrorDetail("Dialog function 'Org_819_Drax_Scavenger_Info' not found");
-        passed = FALSE;
-    };
-
-    // Check if the beer item exists
-    var int beerId; beerId = MEM_GetSymbolIndex("ItFoBeer");
-    if (beerId == -1) {
-        G1CP_TestsuiteErrorDetail("Item 'ItFoBeer' not found");
-        passed = FALSE;
-    };
-
-    // Get variables
-    var int beerGivenPtr; beerGivenPtr = MEM_GetSymbol("drax_bierbekommen");
-    if (!beerGivenPtr) {
-        G1CP_TestsuiteErrorDetail("Variable 'drax_bierbekommen' not found");
-        passed = FALSE;
-    };
-    beerGivenPtr += zCParSymbol_content_offset;
-    var int teachingPtr; teachingPtr = MEM_GetSymbol("drax_Lehrer_frei");
-    if (!teachingPtr) {
-        G1CP_TestsuiteErrorDetail("Variable 'drax_Lehrer_frei' not found");
-        passed = FALSE;
-    };
-    teachingPtr += zCParSymbol_content_offset;
-
-    // Obtain log topic name
-    var string GE_TeacherOW;
-    var int topicNamePtr; topicNamePtr = MEM_GetSymbol("GE_TeacherOW");
-    if (topicNamePtr) {
-        GE_TeacherOW = MEM_ReadString(MEM_ReadInt(topicNamePtr + zCParSymbol_content_offset));
-    } else {
-        G1CP_TestsuiteErrorDetail("Variable 'GE_TeacherOW' not found");
-        passed = FALSE;
-    };
-
-    // At the latest now, we need to stop if there are fails already
-    if (!passed) {
-        return FALSE;
-    };
+    // Get constant values
+    var int GE_TeacherOW; GE_TeacherOW = G1CP_GetStringConst(topicName, 0, 0);
 
     // Rename the log topic if it already exists
     const string tempTopicName = "G1CP Test 27"; // Has to be a unique name with absolute certainty
     G1CP_LogRenameTopic(GE_TeacherOW, tempTopicName);
 
     // Backup values
-    var int beersBefore; beersBefore = Npc_HasItems(hero, beerId);
-    var int beerGivenBak; beerGivenBak = MEM_ReadInt(beerGivenPtr);
-    var int teachingBak; teachingBak = MEM_ReadInt(teachingPtr);
-    var C_Npc slfBak; slfBak = MEM_CpyInst(self);
-    var C_Npc othBak; othBak = MEM_CpyInst(other);
+    var int   beersBefore;  beersBefore  = Npc_HasItems(hero, beerId);
+    var int   beerGivenBak; beerGivenBak = G1CP_GetIntVar(bierBekommenName, 0, 0);
+    var int   teachingBak;  teachingBak  = G1CP_GetIntVar(lehrerFreiName, 0, 0);
+    var C_Npc slfBak;       slfBak       = MEM_CpyInst(self);
+    var C_Npc othBak;       othBak       = MEM_CpyInst(other);
 
-    // Backup self and other
+    // Set self and other
     self  = MEM_CpyInst(hero);
     other = MEM_CpyInst(hero);
 
@@ -82,8 +49,8 @@ func int G1CP_Test_027() {
     if (beersBefore > 0) {
         Npc_RemoveInvItems(hero, beerId, beersBefore);
     };
-    MEM_WriteInt(beerGivenPtr, FALSE);
-    MEM_WriteInt(teachingPtr, FALSE);
+    G1CP_SetIntVar(bierBekommenName, 0, FALSE);
+    G1CP_SetIntVar(lehrerFreiName, 0, FALSE);
 
     // Just run the dialog and see what happens
     MEM_CallByID(funcId);
@@ -93,13 +60,13 @@ func int G1CP_Test_027() {
     AI_StandUpQuick(hero);
 
     // Check the variable and log topic
-    topicCreated = G1CP_LogGetTopic(GE_TeacherOW) != 0;
-    teachingUnlocked = MEM_ReadInt(teachingPtr);
+    topicCreated     = G1CP_LogGetTopic(GE_TeacherOW) != 0;
+    teachingUnlocked = G1CP_GetIntVar(lehrerFreiName, 0, 0);
     if (topicCreated) {
-        G1CP_TestsuiteErrorDetail("Log note 'GE_TeacherOW' was wrongfully created");
+        G1CP_TestsuiteErrorDetailSSS("Log note '", topicName, "' was wrongfully created");
     };
     if (teachingUnlocked) {
-        G1CP_TestsuiteErrorDetail("Variable 'drax_Lehrer_frei' was wrongfully set to true");
+        G1CP_TestsuiteErrorDetailSSS("Variable '", lehrerFreiName, "' was wrongfully set to true");
     };
     pass1passed = (!topicCreated) && (!teachingUnlocked);
 
@@ -110,8 +77,8 @@ func int G1CP_Test_027() {
 
     // Set variables
     CreateInvItem(hero, beerId);
-    MEM_WriteInt(beerGivenPtr, FALSE);
-    MEM_WriteInt(teachingPtr, FALSE);
+    G1CP_SetIntVar(bierBekommenName, 0, FALSE);
+    G1CP_SetIntVar(lehrerFreiName, 0, FALSE);
 
     // Just run the dialog and see what happens
     MEM_CallByID(funcId);
@@ -121,13 +88,13 @@ func int G1CP_Test_027() {
     AI_StandUpQuick(hero);
 
     // Check the variable and log topic
-    topicCreated = G1CP_LogGetTopic(GE_TeacherOW) != 0;
-    teachingUnlocked = MEM_ReadInt(teachingPtr);
+    topicCreated     = G1CP_LogGetTopic(GE_TeacherOW) != 0;
+    teachingUnlocked = G1CP_GetIntVar(lehrerFreiName, 0, 0);
     if (!topicCreated) {
-        G1CP_TestsuiteErrorDetail("Log note 'GE_TeacherOW' was not created");
+        G1CP_TestsuiteErrorDetailSSS("Log note '", topicName, "' was not created");
     };
     if (!teachingUnlocked) {
-        G1CP_TestsuiteErrorDetail("Variable 'drax_Lehrer_frei' was not set to true");
+        G1CP_TestsuiteErrorDetailSSS("Variable '", lehrerFreiName, "' was not set to true");
     };
     pass2passed = (topicCreated) && (teachingUnlocked);
 
@@ -141,9 +108,9 @@ func int G1CP_Test_027() {
     self  = MEM_CpyInst(slfBak);
     other = MEM_CpyInst(othBak);
 
-    // Revert values
-    MEM_WriteInt(teachingPtr, teachingBak);
-    MEM_WriteInt(beerGivenPtr, beerGivenBak);
+    // Restore values
+    G1CP_SetIntVar(bierBekommenName, 0, beerGivenBak);
+    G1CP_SetIntVar(lehrerFreiName, 0, teachingBak);
     G1CP_LogRenameTopic(tempTopicName, GE_TeacherOW);
     if (beersBefore > 0) {
         CreateInvItems(hero, beerId, beersBefore);
