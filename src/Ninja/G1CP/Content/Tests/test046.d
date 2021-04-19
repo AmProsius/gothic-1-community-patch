e5a1261195bfd785af330c5fddb8845eb7738d2b
AmProsius <ff@amprosius.de>
2021-04-19 10:37:26 +0200

    refactor: simplify test of #46

--- src/Ninja/G1CP/Content/Tests/test046.d
+++ src/Ninja/G1CP/Content/Tests/test046.d
@@ -11,33 +11,34 @@
  */
 func void G1CP_Test_046() {
     G1CP_Testsuite_CheckManual();
+    var int funcId; funcId = G1CP_Testsuite_CheckFunc("B_Story_ExploreSunkenTower", "void|none", "");
+    var int guildId; guildId = G1CP_Testsuite_CheckIntConst("GIL_GRD", 0);
+    var int bartholoId; bartholoId = G1CP_Testsuite_CheckInfo("DIA_EBR_106_Bartholo_Wait4SC");
+    var int skipId; skipId = G1CP_Testsuite_CheckInfo("GRD_211_Skip_WELCOME");
+    var zCWaypoint cellarWp; cellarWp = G1CP_Testsuite_FindWaypoint("OCC_CELLAR_BACK_LEFT_CELL");
+    var zCWaypoint staircaseWp; staircaseWp = G1CP_Testsuite_FindWaypoint("OCC_STABLE_STAIRCASE_TOP");
+    G1CP_Testsuite_CheckPassed();
 
-    // Define possibly missing symbols locally
-    const int GIL_GRD = 2;
+    // Get constant values
+    const int GUILD = 0; GUILD = G1CP_GetIntConstI(guildId, 0, 0);
 
     // Two passes
     if (!Npc_HasItems(hero, G1CP_046_SmithDoor_Item)) {
         // First function call (PC doesn't have the key yet): Teleport to the key
 
-        // Find the story function
-        var int funcId; funcId = MEM_GetSymbolIndex("B_Story_ExploreSunkenTower");
-        if (funcId == -1) {
-            G1CP_TestsuiteErrorDetail("Function 'B_Story_ExploreSunkenTower' not found");
-        };
-
         // Advance the story progression
         MEM_CallByID(funcId);
 
         // This dialog would strangely trigger through walls (investigate why! Maybe by "B_CheckForImportantInfo"?)
-        G1CP_SetInfoTold("DIA_EBR_106_Bartholo_Wait4SC", TRUE);
-        G1CP_SetInfoTold("GRD_211_Skip_WELCOME", TRUE);
+        G1CP_SetInfoToldI(bartholoId, TRUE);
+        G1CP_SetInfoToldI(skipId, TRUE);
 
         // Teleport to the cell
-        AI_Teleport(hero, "OCC_CELLAR_BACK_LEFT_CELL");
+        AI_Teleport(hero, cellarWp.name);
 
         // Change the player's guild to not be attacked (just in case)
-        hero.guild = GIL_GRD;
-        Npc_SetTrueGuild(hero, GIL_GRD);
+        hero.guild = GUILD;
+        Npc_SetTrueGuild(hero, GUILD);
 
         // Some information
         Print("Confirm that the key is on the ground, pick it up and call the test again to teleport to the door.");
@@ -46,7 +47,7 @@ func void G1CP_Test_046() {
         // Second function cell (PC does have the key): Teleport
 
         // Allow the player to confirm
-        AI_Teleport(hero, "OCC_STABLE_STAIRCASE_TOP");
+        AI_Teleport(hero, staircaseWp.name);
 
         // Some information
         Print("Drop the key first and check if the door is unlocked already. Confirm that the key opens the door.");
