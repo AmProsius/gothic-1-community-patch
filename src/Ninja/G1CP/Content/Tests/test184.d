d1aea5c0186c078a798da2659f6a47e6f392f917
szapp <dev.szapp@gmail.com>
2021-04-19 22:33:08 +0200

    refactor: simplify test of #184

--- src/Ninja/G1CP/Content/Tests/test184.d
+++ src/Ninja/G1CP/Content/Tests/test184.d
@@ -7,49 +7,29 @@
  * Expected behavior: The condition function will return FALSE.
  */
 func int G1CP_Test_184() {
+    var int funcId; funcId = G1CP_Testsuite_CheckDialogConditionFunc("Info_Bartholo_Krautbote_Condition");
+    var int currierId; currierId = G1CP_Testsuite_CheckIntVar("Kalom_Krautbote", 0);
+    var int deliveredId; deliveredId = G1CP_Testsuite_CheckIntVar("Kalom_DeliveredWeed", 0);
+    G1CP_Testsuite_CheckPassed();
+
     // Define possibly missing symbols locally
     const int LOG_RUNNING = 1;
 
-    // Check status of the test
-    var int passed; passed = TRUE;
-
-    // Find the dialog condition function
-    var int funcId; funcId = G1CP_GetFuncID("Info_Bartholo_Krautbote_Condition", "int|none");
-    if (funcId == -1) {
-        G1CP_TestsuiteErrorDetail("Dialog condition 'Info_Bartholo_Krautbote_Condition' not found");
-        passed = FALSE;
-    };
-
-    // Obtain symbols
-    if (!G1CP_IsIntVar("Kalom_Krautbote", 0)) {
-        G1CP_TestsuiteErrorDetail("Integer variable 'Kalom_Krautbote' not found");
-        passed = FALSE;
-    };
-    if (!G1CP_IsIntVar("Kalom_DeliveredWeed", 0)) {
-        G1CP_TestsuiteErrorDetail("Integer variable 'Kalom_DeliveredWeed' not found");
-        passed = FALSE;
-    };
-
-    // At the latest now, we need to stop if there are fails already
-    if (!passed) {
-        return FALSE;
-    };
-
     // Back up the values
-    var int krautboteBak;     krautboteBak     = G1CP_GetIntVar("Kalom_Krautbote", 0, 0);
-    var int deliveredWeedBak; deliveredWeedBak = G1CP_GetIntVar("Kalom_DeliveredWeed", 0, 0);
+    var int krautboteBak; krautboteBak = G1CP_GetIntVarI(currierId, 0, 0);
+    var int deliveredWeedBak; deliveredWeedBak = G1CP_GetIntVarI(deliveredId, 0, 0);
 
     // Set the variables
-    G1CP_SetIntVar("Kalom_Krautbote",     0, LOG_RUNNING);
-    G1CP_SetIntVar("Kalom_DeliveredWeed", 0, TRUE);
+    G1CP_SetIntVarI(currierId, 0, LOG_RUNNING);
+    G1CP_SetIntVarI(deliveredId, 0, TRUE);
 
     // Call dialog condition function
     G1CP_Testsuite_Call(funcId, 0, 0, FALSE);
     var int ret; ret = MEM_PopIntResult();
 
     // Restore the variables
-    G1CP_SetIntVar("Kalom_Krautbote",     0, krautboteBak);
-    G1CP_SetIntVar("Kalom_DeliveredWeed", 0, deliveredWeedBak);
+    G1CP_SetIntVarI(currierId, 0, krautboteBak);
+    G1CP_SetIntVarI(deliveredId, 0, deliveredWeedBak);
 
     // Check return value
     if (ret) {
