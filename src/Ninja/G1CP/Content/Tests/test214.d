67f1e83eb21640aec1e904f15a9e025cfe6614a4
szapp <dev.szapp@gmail.com>
2021-04-19 22:25:26 +0200

    refactor: simplify test of #214

--- src/Ninja/G1CP/Content/Tests/test214.d
+++ src/Ninja/G1CP/Content/Tests/test214.d
@@ -8,12 +8,16 @@
  */
 func void G1CP_Test_214() {
     G1CP_Testsuite_CheckManual();
+    var zCWaypoint wp; wp = G1CP_Testsuite_FindWaypoint("OCR_OUTSIDE_HUT_77_MOVEMENT");
+    var int paydayId; paydayId = G1CP_Testsuite_CheckIntVar("Bloodwyn_PayDay", 0);
+    var int infoId; infoId = G1CP_Testsuite_CheckInfo("Info_Bloodwyn_Hello");
+    G1CP_Testsuite_CheckPassed();
 
     // Shut up Bloodwyn
-    G1CP_SetIntVar("Bloodwyn_PayDay", 0, Wld_GetDay()+1);
-    G1CP_SetInfoTold("Info_Bloodwyn_Hello", TRUE);
+    G1CP_SetIntVarI(paydayId, 0, Wld_GetDay()+1);
+    G1CP_SetInfoToldI(infoId, TRUE);
 
     // Set time and place
     Wld_SetTime(18, 0);
-    AI_Teleport(hero, "OCR_OUTSIDE_HUT_77_MOVEMENT");
+    AI_Teleport(hero, wp.name);
 };
