003306d14c2575a8ffab81ca984a8f873c94e9e3
AmProsius <ff@amprosius.de>
2021-04-20 08:59:32 +0200

    refactor: simplify test of #92

--- src/Ninja/G1CP/Content/Tests/test091.d
+++ src/Ninja/G1CP/Content/Tests/test091.d
@@ -12,7 +12,7 @@ func void G1CP_Test_091() {
     G1CP_Testsuite_CheckInfo("DIA_Horatio_ThoughtSTR");
     var int helloId; helloId = G1CP_Testsuite_CheckInfo("DIA_Horatio_Hello");
     var int teachStrId; teachStrId = G1CP_Testsuite_CheckInfo("DIA_Horatio_PleaseTeachSTR");
-    var int jeremiahId; jeremiahId = G1CP_Testsuite_CheckInfo("DIA_Jeremiah_Horatio");
+    var int horatioId; horatioId = G1CP_Testsuite_CheckInfo("DIA_Jeremiah_Horatio");
     var int varId; varId = G1CP_Testsuite_CheckIntVar("horatio_StrFree", 0);
     var C_Npc npc; npc = G1CP_Testsuite_FindNpc("Bau_901_Horatio");
     G1CP_Testsuite_CheckPassed();
@@ -20,7 +20,7 @@ func void G1CP_Test_091() {
     // Set unlock the dialog
     G1CP_SetInfoToldI(helloId, TRUE); // Optional
     G1CP_SetInfoToldI(teachStrId, TRUE);
-    G1CP_SetInfoToldI(jeremiahId, TRUE);
+    G1CP_SetInfoToldI(horatioId, TRUE);
     G1CP_SetIntVarI(varId, 0, FALSE);
 
     // Teleport the hero
