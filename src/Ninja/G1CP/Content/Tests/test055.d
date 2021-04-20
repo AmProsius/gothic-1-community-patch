f31e922fe3c1b5ca20a2da4dca7110ce2fe76db2
szapp <dev.szapp@gmail.com>
2021-04-20 22:26:19 +0200

    refactor: simplify test of #55

--- src/Ninja/G1CP/Content/Tests/test055.d
+++ src/Ninja/G1CP/Content/Tests/test055.d
@@ -7,35 +7,23 @@
  * Expected behavior: InExtremo is playing and Grim (if available) will talk to the player about it.
  */
 func void G1CP_Test_055() {
-    if (!G1CP_TestsuiteAllowManual) {
-        return;
-    };
+    G1CP_Testsuite_CheckManual();
+    const int CHAPTER_NUM = 2;
+    var zCWaypoint wp; wp = G1CP_Testsuite_FindWaypoint("OCR_THORUS");
+    var int funcId; funcId = G1CP_Testsuite_CheckFunc("B_Kapitelwechsel", "void|int", "");
+    var int chptrId; chptrId = G1CP_Testsuite_CheckIntVar("Kapitel", 0);
+    G1CP_Testsuite_CheckPassed();
 
-    // Check for symbols
-    var int passed; passed = TRUE;
-    var int funcId; funcId = G1CP_GetFuncID("B_Kapitelwechsel", "void|int");
-    if (funcId == -1) {
-        G1CP_TestsuiteErrorDetail("Function 'B_Kapitelwechsel' not found");
-        passed = FALSE;
-    };
-    if (!G1CP_IsIntVar("Kapitel", 0)) {
-        G1CP_TestsuiteErrorDetail("Integer variable 'Kapitel' not found");
-        passed = FALSE;
-    } else if (G1CP_GetIntVar("Kapitel", 0, 0) >= 2) {
+    if (G1CP_GetIntVarI(chptrId, 0, 0) >= 2) {
         G1CP_TestsuiteErrorDetail("It must be chapter one to run this test");
-        passed = FALSE;
-    };
-
-    // Leave here at the latest
-    if (!passed) {
         return;
     };
 
     // Start the second chapter
-    MEM_PushIntParam(2);
+    MEM_PushIntParam(CHAPTER_NUM);
     MEM_CallByID(funcId);
 
     // Teleport the player
     Wld_SetTime(10, 0);
-    AI_Teleport(hero, "OCR_THORUS");
+    AI_Teleport(hero, wp.name);
 };
