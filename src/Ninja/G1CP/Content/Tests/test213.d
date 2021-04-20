17e26a324f4229d21caa8965cc6fc7c770cc1d8b
szapp <dev.szapp@gmail.com>
2021-04-20 22:26:37 +0200

    refactor: simplify test of #213

--- src/Ninja/G1CP/Content/Tests/test213.d
+++ src/Ninja/G1CP/Content/Tests/test213.d
@@ -6,16 +6,10 @@
  * Expected behavior: The cauldron is not usable anymore (without a spoon).
  */
 func void G1CP_Test_213() {
-    if (!G1CP_TestsuiteAllowManual) {
-        return;
-    };
-
-    // Check for the item
-    var int itemId; itemId = G1CP_GetItemInstID("ITMISCOOP");
-    if (itemId == -1) {
-        G1CP_TestsuiteErrorDetail("Item 'ItMiScoop' not found");
-        return;
-    };
+    G1CP_Testsuite_CheckManual();
+    var zCWaypoint wp; wp = G1CP_Testsuite_FindWaypoint("NC_HUT21_OUT");
+    var int itemId; itemId = G1CP_Testsuite_CheckItem("ITMISCOOP");
+    G1CP_Testsuite_CheckPassed();
 
     // Give the player item to check
     if (!Npc_HasItems(hero, itemId)) {
@@ -23,5 +17,5 @@ func void G1CP_Test_213() {
     };
 
     // Teleport the player to the MOB
-    AI_Teleport(hero, "NC_HUT21_OUT");
+    AI_Teleport(hero, wp.name);
 };
