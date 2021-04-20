781a03ad7af282bd29ad09690800edb23e02a504
szapp <dev.szapp@gmail.com>
2021-04-20 22:13:08 +0200

    refactor: make test of #126 automatic

--- src/Ninja/G1CP/Content/Tests/test126.d
+++ src/Ninja/G1CP/Content/Tests/test126.d
@@ -1,24 +1,18 @@
 /*
  * #126 The trading menu is opened after Sharky is appointed as new fence
  *
- * There does not seem an easy way to test this fix programmatically, so this test relies on manual confirmation.
+ * The "trade" property of the info is checked.
  *
- * Expected behavior: Sharky will not open the trade menu when talking about Fisk's quest.
+ * Expected behavior: Sharky's dialog about Fisk's quest will not open the trade menu.
  */
-func void G1CP_Test_126() {
-    G1CP_Testsuite_CheckManual();
-    var int infoId; infoId = G1CP_Testsuite_CheckInfo("Org_843_Sharky_Fisk");
-    var int fenceId; fenceId = G1CP_Testsuite_CheckIntVar("Fisk_GetNewHehler", 0);
-    var C_Npc npc; npc = G1CP_Testsuite_FindNpc("Org_843_Sharky");
+func int G1CP_Test_126() {
+    var oCInfo info; info = G1CP_Testsuite_FindInfo("Org_843_Sharky_Fisk");
     G1CP_Testsuite_CheckPassed();
 
-    // Define possibly missing symbols locally
-    const int LOG_RUNNING = 1;
-
-    // Enable the dialog by setting the quest marker to running
-    G1CP_SetIntVarI(fenceId, 0, LOG_RUNNING);
-    G1CP_SetInfoToldI(infoId, FALSE);
-
-    // Teleport the hero to the NPC
-    AI_Teleport(hero, npc.wp);
+    if (info.trade) {
+        G1CP_TestsuiteErrorDetail("Dialog is a trade dialog");
+        return FALSE;
+    } else {
+        return TRUE;
+    };
 };
