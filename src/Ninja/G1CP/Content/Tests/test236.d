66dc199eafbfb32f7a624acbbd85cd40ca02cc66
AmProsius <ff@amprosius.de>
2021-05-08 21:57:03 +0200

    feat: correct name of orc dog in guild assignment

---
+++ src/Ninja/G1CP/Content/Tests/test236.d
@@ -0,0 +1,11 @@
+/*
+ * #236 Spelling - Guild: "Orkhund" (DE)
+ */
+func int G1CP_Test_236() {
+    G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
+    const int GIL_ORCDOG = 0; GIL_ORCDOG = G1CP_Testsuite_GetIntConst("GIL_ORCDOG", 0);
+    var string name; name = G1CP_Testsuite_GetStringConst("TXT_GUILDS", GIL_ORCDOG);
+    G1CP_Testsuite_CheckPassed();
+
+    return Hlp_StrCmp(name, "Orkhund");
+};
