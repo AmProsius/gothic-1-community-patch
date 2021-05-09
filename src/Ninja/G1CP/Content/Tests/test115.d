199dc5dd4fafd358ebbf8f06e41087d5c8645d34
AmProsius <ff@amprosius.de>
2021-05-09 13:48:31 +0200

    refactor: simplify tests of #32, #115, #129 and #136

--- src/Ninja/G1CP/Content/Tests/test115.d
+++ src/Ninja/G1CP/Content/Tests/test115.d
@@ -7,8 +7,6 @@
  */
 func void G1CP_Test_115() {
     G1CP_Testsuite_CheckManual();
-    const string WORLD = "ORCGRAVEYARD.ZEN";
-    const string WP = "GRYD_001";
 
     // Define possibly missing symbols locally
     const int NPC_FLAG_IMMORTAL = 1 << 1;
@@ -17,12 +15,5 @@ func void G1CP_Test_115() {
     hero.flags = hero.flags | NPC_FLAG_IMMORTAL;
 
     // Teleport the player to the entrance of the orc graveyard
-    if (!Hlp_StrCmp(MEM_World.worldFilename, WORLD)) {
-        const int oCGame__TriggerChangeLevel = 6542464; //0x63D480
-        CALL_zStringPtrParam(WP);
-        CALL_zStringPtrParam(WORLD);
-        CALL__thiscall(_@(MEM_Game), oCGame__TriggerChangeLevel);
-    } else {
-        AI_Teleport(hero, WP);
-    };
+    G1CP_TeleportToWorld("ORCGRAVEYARD.ZEN", "GRYD_001");
 };
