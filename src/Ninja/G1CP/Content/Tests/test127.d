4b308d7c02d125fa1993aa07f30bf4385c045852
AmProsius <ff@amprosius.de>
2021-07-31 22:23:09 +0200

    Refactor comment from test of #127

--- src/Ninja/G1CP/Content/Tests/test127.d
+++ src/Ninja/G1CP/Content/Tests/test127.d
@@ -3,19 +3,19 @@
  *
  * There does not seem an easy way to test this fix programmatically, so this test relies on manual confirmation.
  *
- * Expected behavior: The cauldron is not usable anymore (without a spoon).
+ * Expected behavior: The chest can be picked with the combination "LRL".
  */
 func void G1CP_Test_127() {
     G1CP_Testsuite_CheckManual();
     var zCWaypoint wp; wp = G1CP_Testsuite_FindWaypoint("NC_HUT28_OUT");
-    var int itemId; itemId = G1CP_Testsuite_CheckItem("itkelockpick");
+    var int itemId; itemId = G1CP_Testsuite_CheckItem("ITKELOCKPICK");
     G1CP_Testsuite_CheckPassed();
 
-    // Give the player item to check
+    // Give the player an item to check
     if (!Npc_HasItems(hero, itemId)) {
         CreateInvItem(hero, itemId);
     };
 
-    // Teleport the player to the MOB
+    // Teleport the player to the hut
     AI_Teleport(hero, wp.name);
 };
