011f27cebe571d14ebbe7aa2a0cab89269efa3d2
fba <fba@cleverreach.com>
2023-01-16 21:40:40 +0100

    Refactor forwarded call

--- src/Ninja/G1CP/Content/Misc/item.d
+++ src/Ninja/G1CP/Content/Misc/item.d
@@ -1,17 +1,18 @@
 /*
- * Change the value of an item's int variable. Returns true on success.
+ * Forward the call to "B_GiveInvItems"
  */
 func void G1CP_GiveInvItems(var int senderId, var int receiverId, var int itemId, var int amount) {
-    var int funcId; funcId = MEM_GetSymbolIndex("B_GiveInvItems");
-    if (funcId != -1) {
-        MEM_PushInstParam(senderId);
-        MEM_PushInstParam(receiverId);
-        MEM_PushIntParam(itemId);
-        MEM_PushIntParam(amount);
-        MEM_CallById(funcId);
-    } else {
-        // In case "B_GiveInvItems" does not exist
+    var int funcId; funcId = G1CP_GetFuncId("B_GiveInvItems", "void|inst|inst|int|int");
+
+    if (funcId == -1) {
         Npc_RemoveInvItems(senderId, itemId, amount);
         CreateInvItems(receiverId, itemId, amount);
-    };
+        return;
+    }
+
+    MEM_PushInstParam(senderId);
+    MEM_PushInstParam(receiverId);
+    MEM_PushIntParam(itemId);
+    MEM_PushIntParam(amount);
+    MEM_CallById(funcId);
 }
