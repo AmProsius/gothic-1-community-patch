622b8f65fea03d88c7948dfab5b0137f879343ed
Fabian B <ff@amprosius.de>
2024-03-21 13:13:05 +0100

    Revert routine fixes refactoring

--- src/Ninja/G1CP/Content/Fixes/Session/fix214_GrahamDailyRoutine.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix214_GrahamDailyRoutine.d
@@ -2,5 +2,7 @@
  * #214 Graham doesn't sit at campfire
  */
 func int G1CP_214_GrahamDailyRoutine() {
-    return G1CP_RtnChangeWp("Rtn_start_573", "OCR_OUSIDE_HUT_77_INSERT", "OCR_OUTSIDE_HUT_77_INSERT");
+    var int funcId; funcId = G1CP_GetFuncId("Rtn_start_573", "void|none");
+
+    return (G1CP_ReplacePushStr(funcId, 0, "OCR_OUSIDE_HUT_77_INSERT", "OCR_OUTSIDE_HUT_77_INSERT") > 0);
 };
--- src/Ninja/G1CP/Content/Fixes/Session/fix216_DiggerDailyRoutine.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix216_DiggerDailyRoutine.d
@@ -2,5 +2,7 @@
  * #216 Digger doesn't repair hut
  */
 func int G1CP_216_DiggerDailyRoutine() {
-    return G1CP_RtnChangeWp("Rtn_start_506", "OCR_Hut_15", "OCR_HUT_15");
+    var int funcId; funcId = G1CP_GetFuncId("Rtn_start_506", "void|none");
+
+    return (G1CP_ReplacePushStr(funcId, 0, "OCR_Hut_15", "OCR_HUT_15") > 0);
 };
