f9accdf837d57815dcba6635c202de2f8bb6d30d
Fabian B <ff@amprosius.de>
2024-03-21 14:00:18 +0100

    Fix parameter types

--- src/Ninja/G1CP/Content/Misc/item.d
+++ src/Ninja/G1CP/Content/Misc/item.d
@@ -1,17 +1,17 @@
 /*
  * Forward the call to "B_GiveInvItems"
  */
-func void G1CP_GiveInvItems(var int senderId, var int receiverId, var int itemId, var int amount) {
+func void G1CP_GiveInvItems(var C_Npc sender, var C_Npc receiver, var int itemId, var int amount) {
     var int funcId; funcId = G1CP_GetFuncId("B_GiveInvItems", "void|inst|inst|int|int");
 
     if (funcId == -1) {
-        Npc_RemoveInvItems(senderId, itemId, amount);
-        CreateInvItems(receiverId, itemId, amount);
+        Npc_RemoveInvItems(sender, itemId, amount);
+        CreateInvItems(receiver, itemId, amount);
         return;
-    }
+    };
 
-    MEM_PushInstParam(senderId);
-    MEM_PushInstParam(receiverId);
+    MEM_PushInstParam(sender);
+    MEM_PushInstParam(receiver);
     MEM_PushIntParam(itemId);
     MEM_PushIntParam(amount);
     MEM_CallById(funcId);
