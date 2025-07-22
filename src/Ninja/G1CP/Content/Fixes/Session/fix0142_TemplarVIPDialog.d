09ccdedee5b16f452d5a9f9b11eca447944562fe
Fabian Bastwöste <ff@amprosius.de>
2025-07-22 20:27:39 +0200

    Fix change dialog line speaker template

--- src/Ninja/G1CP/Content/Fixes/Session/fix0142_TemplarVIPDialog.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0142_TemplarVIPDialog.d
@@ -3,6 +3,5 @@
  */
 func int G1CP_0142_TemplarVIPDialog() {
     var int funcId; funcId = G1CP_GetFuncId("Info_Tpl_8_WichtigePersonen_Info", "void|none");
-
     return (G1CP_ReplaceOuInst(funcId, 0, "Info_Tpl_8_WichtigePersonen_15_02", self, other, other, self) > 0);
 };
