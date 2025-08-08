8351d8bb7f0f34a48f8cf496f979c9d29f19eda1
szapp <dev.szapp@gmail.com>
2025-08-08 18:19:27 +0200

    Remove previously added empty line
    Ref #415

--- templates/renameNpc/test.d
+++ templates/renameNpc/test.d
@@ -5,7 +5,6 @@ func int G1CP_Test_{ISSUE_NUM_PAD}() {
     {LANGCHECK}
     const string npcInst = "### TODO NPC instance ###";
     const string correctedName = "### TODO Correct name ###";
-
     var C_Npc npc; npc = G1CP_Testsuite_CreateNpc(npcInst);
     G1CP_Testsuite_CheckPassed();
 
