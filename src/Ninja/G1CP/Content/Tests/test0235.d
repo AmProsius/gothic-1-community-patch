8ca3ae068b9a71e022c0f4f18e8041e2b02f6ed7
szapp <dev.szapp@gmail.com>
2025-08-08 18:45:06 +0200

    Fix broken tests from previous commits
    Ref #415

--- src/Ninja/G1CP/Content/Tests/test0235.d
+++ src/Ninja/G1CP/Content/Tests/test0235.d
@@ -4,7 +4,7 @@
 func int G1CP_Test_0235() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     const string SPELL_NAME = "SPL_TRF_ORCDOG";
-    const int SPL_TRF_ORCDOG = 0; SPL_TRF_ORCDOG = G1CP_Testsuite_GetIntConst(SPELL_NAME, -1);
+    const int SPL_TRF_ORCDOG = 0; SPL_TRF_ORCDOG = G1CP_Testsuite_GetIntConst(SPELL_NAME, 0);
     var string expected; expected = "Verwandlung Orkhund";
     var string name; name = G1CP_Testsuite_GetStringConst("TXT_SPELLS", SPL_TRF_ORCDOG);
     G1CP_Testsuite_CheckPassed();
--- src/Ninja/G1CP/Content/Tests/test0236.d
+++ src/Ninja/G1CP/Content/Tests/test0236.d
@@ -4,7 +4,7 @@
 func int G1CP_Test_0236() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     const string GUILD_NAME = "GIL_ORCDOG";
-    const int GIL_ORCDOG = 0; GIL_ORCDOG = G1CP_Testsuite_GetIntConst(GUILD_NAME, -1);
+    const int GIL_ORCDOG = 0; GIL_ORCDOG = G1CP_Testsuite_GetIntConst(GUILD_NAME, 0);
     var string expected; expected = "Orkhund";
     var string name; name = G1CP_Testsuite_GetStringConst("TXT_GUILDS", GIL_ORCDOG);
     G1CP_Testsuite_CheckPassed();
