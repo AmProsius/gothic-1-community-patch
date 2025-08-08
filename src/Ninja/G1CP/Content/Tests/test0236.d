626196e317792cb6191064cd89ef5914c6132888
szapp <dev.szapp@gmail.com>
2025-08-08 17:49:06 +0200

    Refactor constant assignments in tests
    Ref #415

--- src/Ninja/G1CP/Content/Tests/test0236.d
+++ src/Ninja/G1CP/Content/Tests/test0236.d
@@ -3,12 +3,13 @@
  */
 func int G1CP_Test_0236() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
-    const string GUILD_NAME = ""; GUILD_NAME = "GIL_ORCDOG";
-    const int GIL_ORCDOG = 0; GIL_ORCDOG = G1CP_Testsuite_GetIntConst(GUILD_NAME, 0);
+    const string GUILD_NAME = "GIL_ORCDOG";
+    const int GIL_ORCDOG = 0; GIL_ORCDOG = G1CP_Testsuite_GetIntConst(GUILD_NAME, -1);
+    var string expected; expected = "Orkhund";
     var string name; name = G1CP_Testsuite_GetStringConst("TXT_GUILDS", GIL_ORCDOG);
     G1CP_Testsuite_CheckPassed();
 
-    if (!Hlp_StrCmp(name, "Orkhund")) {
+    if (!Hlp_StrCmp(name, expected)) {
         G1CP_TestsuiteErrorDetailSSSSS("String incorrect: TXT_GUILDS[", GUILD_NAME, "] = '", name, "'");
         return FALSE;
     };
