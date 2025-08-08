f904162db9b65998dc8cc1b00a0825b1c7578388
Fabian Bastwöste <ff@amprosius.de>
2025-08-08 20:33:21 +0200

    Update comments in fix template

--- templates/changeDialogLineSpeaker/session.d
+++ templates/changeDialogLineSpeaker/session.d
@@ -2,10 +2,6 @@
  * #{ISSUE_NUM} {LONGNAME}
  */
 func int G1CP_{ISSUE_NUM_PAD}_{SHORTNAME}() {
-    // ### TODO: Replace info name of "Xxx_Info" ###
-    // ### TODO: Replace OU name of "Xxx_Ou" ###
-    // ### TODO: Optional: Switch `other` and `self` ###
-
-    var int funcId; funcId = G1CP_GetFuncId("Xxx_Info", "void|none");
-    return (G1CP_ReplaceOuInst(funcId, 0, "Xxx_Ou", self, other, other, self) > 0);
+    var int funcId; funcId = G1CP_GetFuncId("### TODO info function ###", "void|none");
+    return (G1CP_ReplaceOuInst(funcId, 0, "### TODO OU name ###", self, other, other, self) > 0);  // ### TODO Adjust ordering of self, other ###
 };
