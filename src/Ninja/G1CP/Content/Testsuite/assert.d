a5822cbcfb873cae41c024699d4ddbd8182b03ec
szapp <dev.szapp@gmail.com>
2025-08-24 21:25:10 +0200

    Add not-equal test suite assertion

--- src/Ninja/G1CP/Content/Testsuite/assert.d
+++ src/Ninja/G1CP/Content/Testsuite/assert.d
@@ -39,13 +39,27 @@ func void G1CP_Testsuite_AssertS(var string actual, var string expected) {
 /*
  * Assert integer inequalities.
  */
+func void G1CP_Testsuite_AssertNe(var int actual, var int expected) {
+    G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
+
+    if (actual == expected) {
+        if (SB_New()) {
+            SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
+            SB(" expected !"); SBi(expected); SB(",");
+            SB(" got "); SBi(actual); SB(".");
+            G1CP_TestsuiteErrorDetail(SB_ToString());
+            SB_Destroy();
+        };
+        G1CP_Testsuite_ForceTestToReturn(FALSE);
+    };
+};
 func void G1CP_Testsuite_AssertGt(var int actual, var int expected) {
     G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
 
     if (!(actual > expected)) {
         if (SB_New()) {
             SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
-            SB(" expected greater than "); SBi(expected); SB(", ");
+            SB(" expected greater than "); SBi(expected); SB(",");
             SB(" got "); SBi(actual); SB(".");
             G1CP_TestsuiteErrorDetail(SB_ToString());
             SB_Destroy();
@@ -59,7 +73,7 @@ func void G1CP_Testsuite_AssertGe(var int actual, var int expected) {
     if (!(actual >= expected)) {
         if (SB_New()) {
             SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
-            SB(" expected greater than or equal to "); SBi(expected); SB(", ");
+            SB(" expected greater than or equal to "); SBi(expected); SB(",");
             SB(" got "); SBi(actual); SB(".");
             G1CP_TestsuiteErrorDetail(SB_ToString());
             SB_Destroy();
@@ -73,7 +87,7 @@ func void G1CP_Testsuite_AssertLt(var int actual, var int expected) {
     if (!(actual < expected)) {
         if (SB_New()) {
             SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
-            SB(" expected less than "); SBi(expected); SB(", ");
+            SB(" expected less than "); SBi(expected); SB(",");
             SB(" got "); SBi(actual); SB(".");
             G1CP_TestsuiteErrorDetail(SB_ToString());
             SB_Destroy();
@@ -87,7 +101,7 @@ func void G1CP_Testsuite_AssertLe(var int actual, var int expected) {
     if (!(actual <= expected)) {
         if (SB_New()) {
             SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
-            SB(" expected less than or equal to "); SBi(expected); SB(", ");
+            SB(" expected less than or equal to "); SBi(expected); SB(",");
             SB(" got "); SBi(actual); SB(".");
             G1CP_TestsuiteErrorDetail(SB_ToString());
             SB_Destroy();
