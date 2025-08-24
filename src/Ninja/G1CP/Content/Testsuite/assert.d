d837c25fda74d5cbb0409394cbf114ebe4518a86
szapp <dev.szapp@gmail.com>
2025-08-24 16:19:08 +0200

    Add integer inequality assertions to test suite

--- src/Ninja/G1CP/Content/Testsuite/assert.d
+++ src/Ninja/G1CP/Content/Testsuite/assert.d
@@ -5,7 +5,6 @@ func void G1CP_Testsuite_Assert(var int actual, var int expected) {
     G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
 
     if (expected != actual) {
-        // Retrieve the number of asserts
         if (SB_New()) {
             SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
             SB(" expected "); SBi(expected); SB(",");
@@ -25,11 +24,71 @@ func void G1CP_Testsuite_AssertS(var string actual, var string expected) {
     G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
 
     if (STR_Compare(expected, actual) != STR_EQUAL) {
-        // Retrieve the number of asserts
         if (SB_New()) {
             SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
             SB(" expected '"); SB(expected); SB("',");
             SB(" got '"); SB(actual); SB("'.");
+            G1CP_TestsuiteErrorDetail(SB_ToString());
+            SB_Destroy();
+        };
+        G1CP_Testsuite_ForceTestToReturn(FALSE);
+    };
+};
+
+
+/*
+ * Assert integer inequalities.
+ */
+func void G1CP_Testsuite_AssertGt(var int actual, var int expected) {
+    G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
+
+    if (!(actual > expected)) {
+        if (SB_New()) {
+            SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
+            SB(" expected greater than "); SBi(expected); SB(", ");
+            SB(" got "); SBi(actual); SB(".");
+            G1CP_TestsuiteErrorDetail(SB_ToString());
+            SB_Destroy();
+        };
+        G1CP_Testsuite_ForceTestToReturn(FALSE);
+    };
+};
+func void G1CP_Testsuite_AssertGe(var int actual, var int expected) {
+    G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
+
+    if (!(actual >= expected)) {
+        if (SB_New()) {
+            SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
+            SB(" expected greater than or equal to "); SBi(expected); SB(", ");
+            SB(" got "); SBi(actual); SB(".");
+            G1CP_TestsuiteErrorDetail(SB_ToString());
+            SB_Destroy();
+        };
+        G1CP_Testsuite_ForceTestToReturn(FALSE);
+    };
+};
+func void G1CP_Testsuite_AssertLt(var int actual, var int expected) {
+    G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
+
+    if (!(actual < expected)) {
+        if (SB_New()) {
+            SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
+            SB(" expected less than "); SBi(expected); SB(", ");
+            SB(" got "); SBi(actual); SB(".");
+            G1CP_TestsuiteErrorDetail(SB_ToString());
+            SB_Destroy();
+        };
+        G1CP_Testsuite_ForceTestToReturn(FALSE);
+    };
+};
+func void G1CP_Testsuite_AssertLe(var int actual, var int expected) {
+    G1CP_TestsuiteAssertNum += 1; // Keep track of which assertion failed
+
+    if (!(actual <= expected)) {
+        if (SB_New()) {
+            SB("Assertion #"); SBi(G1CP_TestsuiteAssertNum);
+            SB(" expected less than or equal to "); SBi(expected); SB(", ");
+            SB(" got "); SBi(actual); SB(".");
             G1CP_TestsuiteErrorDetail(SB_ToString());
             SB_Destroy();
         };
