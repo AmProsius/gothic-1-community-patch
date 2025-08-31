5e9ec187bdcb4e7a944a1d1c27c8b7f9672ec0a6
szapp <dev.szapp@gmail.com>
2025-08-31 09:57:30 +0200

    Reduce the fix and test #31 to its minimum

--- src/Ninja/G1CP/Content/Fixes/Session/fix0031_WolfPlateDialog.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0031_WolfPlateDialog.d
@@ -2,21 +2,18 @@
  * #31 Wolf's minecrawler plate dialog doesn't disappear
  */
 func int G1CP_0031_WolfPlateDialog() {
-    if (G1CP_IsFunc("Info_Wolf_MCPLATESFEW_Condition", "int|none"))
-    && (G1CP_IsFunc("Info_Wolf_MCPLATESENOUGH_Condition", "int|none"))
-    && (G1CP_IsInfoInst("Info_Wolf_MCPLATESENOUGH")) {
-        HookDaedalusFuncS("Info_Wolf_MCPLATESFEW_Condition", "G1CP_0031_WolfPlateDialog_Hook1");
-        HookDaedalusFuncS("Info_Wolf_MCPLATESENOUGH_Condition", "G1CP_0031_WolfPlateDialog_Hook2");
-        return TRUE;
-    } else {
+    if (!G1CP_IsFunc("Info_Wolf_MCPLATESFEW_Condition", "int|none"))
+    || (!G1CP_IsInfoInst("Info_Wolf_MCPLATESENOUGH")) {
         return FALSE;
     };
+
+    HookDaedalusFuncS("Info_Wolf_MCPLATESFEW_Condition", "G1CP_0031_WolfPlateDialog_Hook");
 };
 
 /*
  * This function intercepts the dialog condition to introduce more conditions
  */
-func int G1CP_0031_WolfPlateDialog_Hook1() {
+func int G1CP_0031_WolfPlateDialog_Hook() {
     G1CP_ReportFuncToSpy();
 
     // Add the new condition (other conditions remain untouched)
@@ -24,21 +21,5 @@ func int G1CP_0031_WolfPlateDialog_Hook1() {
         return FALSE;
     };
 
-    // Continue with the original function
-    ContinueCall();
-};
-
-/*
- * Exact copy of the function above. Need unique functions for both because of the way Daedalus hooks work
- */
-func int G1CP_0031_WolfPlateDialog_Hook2() {
-    G1CP_ReportFuncToSpy();
-
-    // Add the new condition (other conditions remain untouched)
-    if (Npc_KnowsInfo(hero, MEM_GetSymbolIndex("Info_Wolf_MCPLATESENOUGH"))) {
-        return FALSE;
-    };
-
-    // Continue with the original function
     ContinueCall();
 };
