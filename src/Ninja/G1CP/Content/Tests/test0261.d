f28a11d325cebabd8d1dcf9776a33f9beef3d068
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 17:45:21 +0200

    Apply suggestions from code review
    Co-authored-by: Sören Zapp <github.zr737@slmail.me>

--- src/Ninja/G1CP/Content/Tests/test0261.d
+++ src/Ninja/G1CP/Content/Tests/test0261.d
@@ -1,8 +1,8 @@
 /*
  * #261 Spelling - Orc Hunter (DE)
  */
-func int G1CP_Test_0261() {
+func void G1CP_Test_0261() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
-    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("Mst_Default_OrcScout");
+    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("OrcScout");
     G1CP_Testsuite_AssertS(npc.name, "Ork-Jäger");
 };
