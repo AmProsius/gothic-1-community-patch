7a18a72588dc4788dfb40dce0a151cde8b581b2c
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 09:16:33 +0200

    Fix Ulbert's storage chest key description
    Refs #266

---
+++ src/Ninja/G1CP/Content/Tests/test0266.d
@@ -0,0 +1,20 @@
+/*
+ * #266 Spelling - Old Mine storage chest key (DE/EN)
+ *
+ * The name of the item "ItKe_OM_03" is inspected programmatically.
+ *
+ * Expected behavior: The key's description will have the correct capitalization
+ * (checked for English and German localization only).
+ */
+func int G1CP_Test_0266() {
+    G1CP_Testsuite_CheckLang(G1CP_Lang_EN | G1CP_Lang_DE);
+    var C_Item itm; itm = G1CP_Testsuite_CreateItem("ItKe_OM_03");
+
+    if (G1CP_Lang & G1CP_Lang_EN) {
+        return G1CP_Testsuite_InspectItemString(itm, "text[0]", "Opens the chests in the");
+    } else if (G1CP_Lang & G1CP_Lang_DE) {
+        return G1CP_Testsuite_InspectItemString(itm, "text[0]", "Ã–ffnet die Truhen");
+    };
+
+    return false;
+};
