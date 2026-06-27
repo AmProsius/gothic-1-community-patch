28f344869f91e90403fe6e0f44d069716be5f9fe
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 17:05:08 +0200

    Apply suggestions from code review
    Co-authored-by: Sören Zapp <github.zr737@slmail.me>

--- src/Ninja/G1CP/Content/Fixes/Session/fix0399_DE_SkeletonMageName.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0399_DE_SkeletonMageName.d
@@ -2,10 +2,5 @@
  * #399 Spelling - Skeleton Mage (DE)
  */
 func int G1CP_0399_DE_SkeletonMageName() {
-    return G1CP_Tpl_ChangeNpcStrVar(
-        "SkeletonMage",
-        "name",
-        "Skelett Magier",
-        "Skelettmagier"
-    );
+    return G1CP_Tpl_ChangeNpcStrVar("SkeletonMage", "name", "Skelett Magier", "Skelettmagier");
 };
