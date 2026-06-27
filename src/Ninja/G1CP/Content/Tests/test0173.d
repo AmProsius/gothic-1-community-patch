8ef0879d117b06416e35b37c4c9414d64c61dbc2
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 14:17:54 +0200

    Fix #173
    Refs #271

--- src/Ninja/G1CP/Content/Tests/test0173.d
+++ src/Ninja/G1CP/Content/Tests/test0173.d
@@ -4,5 +4,5 @@
 func void G1CP_Test_0173() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     var C_Item itm; itm = G1CP_Testsuite_CreateItem("ItKe_Gomez_01");
-    G1CP_Testsuite_AssertS(itm.text, "Öffnet Gomez' Truhen");
+    G1CP_Testsuite_AssertS(itm.text, "Öffnet Gomez' Truhen.");
 };
