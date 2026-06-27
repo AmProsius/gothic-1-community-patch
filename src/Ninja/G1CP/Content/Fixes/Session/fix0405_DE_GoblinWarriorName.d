7b02c0c2096134d6457e352d5558d18ac4cdfa86
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 16:03:46 +0200

    Add values to fix template

--- src/Ninja/G1CP/Content/Fixes/Session/fix0405_DE_GoblinWarriorName.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0405_DE_GoblinWarriorName.d
@@ -3,9 +3,9 @@
  */
 func int G1CP_0405_DE_GoblinWarriorName() {
     return G1CP_Tpl_ChangeNpcStrVar(
-        "### TODO NPC instance name ###",
+        "BlackGobboWarrior",
         "name",
-        "### TODO Original name ###",
-        "### TODO Correct name ###"
+        "Goblin Krieger",
+        "Goblinkrieger"
     );
 };
--- src/Ninja/G1CP/Content/Tests/test0405.d
+++ src/Ninja/G1CP/Content/Tests/test0405.d
@@ -4,6 +4,6 @@
 func int G1CP_Test_0405() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     
-    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("### TODO NPC instance ###");
-    G1CP_Testsuite_AssertS(npc.name, "### TODO Correct name ###");
+    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("BlackGobboWarrior");
+    G1CP_Testsuite_AssertS(npc.name, "Goblinkrieger");
 };
