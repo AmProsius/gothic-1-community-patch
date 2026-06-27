2a886a3847d1532a7dbd01e24b0752958c3c22b6
szapp <dev.szapp@gmail.com>
2026-06-27 19:46:08 +0200

    Update changeDialogSubtitles template

--- templates/changeDialogSubtitles/session.d
+++ templates/changeDialogSubtitles/session.d
@@ -2,9 +2,9 @@
  * #{ISSUE_NUM} {LONGNAME}
  */
 func int G1CP_{ISSUE_NUM_PAD}_{SHORTNAME}() {
-    // ### TODO: Replace OU name of "XXX_Ou" ###
-    const string originalStr = "### TODO: Original string ###";
-    const string correctedStr = "### TODO: Corrected string ###";
-
-    return G1CP_ReplaceOuText("XXX_Ou", originalStr, correctedStr);
+    return G1CP_ReplaceOuText(
+        "### TODO Output unit name ###",
+        "### TODO Original subtitles ###",
+        "### TODO Corrected subtitles ###"
+    );
 };
--- templates/changeDialogSubtitles/test.d
+++ templates/changeDialogSubtitles/test.d
@@ -2,10 +2,7 @@
  * #{ISSUE_NUM} {LONGNAME}
  */
 func int G1CP_Test_{ISSUE_NUM_PAD}() {
-    // ### TODO: Replace OU name of "XXX_Ou" ###
-    // ### TODO: Replace corrected string of "Corrected" ###
     {LANGCHECK}
-    var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("XXX_Ou");
-
-    return G1CP_Testsuite_InspectOuString(ouPtr, "text", "Corrected");
+    var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("### TODO Output unit name ###");
+    return G1CP_Testsuite_InspectOuString(ouPtr, "text", "### TODO Corrected subtitles ###");
 };
