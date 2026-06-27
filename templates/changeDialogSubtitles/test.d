bd542670ce294fd6a9d812f0cf1efbe05e0452fd
szapp <dev.szapp@gmail.com>
2026-06-27 20:41:02 +0200

    Update the changeDialogSubtitles template

--- templates/changeDialogSubtitles/test.d
+++ templates/changeDialogSubtitles/test.d
@@ -4,5 +4,5 @@
 func int G1CP_Test_{ISSUE_NUM_PAD}() {
     {LANGCHECK}
     var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("### TODO Output unit name ###");
-    return G1CP_Testsuite_InspectOuString(ouPtr, "text", "### TODO Corrected subtitles ###");
+    G1CP_Testsuite_AssertS(G1CP_GetOuTextP(ouPtr), "### TODO Corrected subtitles ###");
 };
