b18ca3748ac631d175c7eaadd90b282703e4818a
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 17:43:43 +0200

    Add todo values

--- src/Ninja/G1CP/Content/Fixes/Session/fix0401_DE_SkeletonScoutName.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0401_DE_SkeletonScoutName.d
@@ -2,10 +2,5 @@
  * #401 Spelling - Skeleton Scout (DE)
  */
 func int G1CP_0401_DE_SkeletonScoutName() {
-    return G1CP_Tpl_ChangeNpcStrVar(
-        "### TODO NPC instance name ###",
-        "name",
-        "### TODO Original name ###",
-        "### TODO Correct name ###"
-    );
+    return G1CP_Tpl_ChangeNpcStrVar("SkeletonScout", "name", "Skelett Späher", "Skelettspäher");
 };
