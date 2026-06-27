4bf751394305a5d23e98ca0e38903dacb2ec5b9b
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 17:04:44 +0200

    Apply suggestions from code review
    Co-authored-by: Sören Zapp <github.zr737@slmail.me>

--- src/Ninja/G1CP/Content/Fixes/Session/fix0405_DE_GoblinWarriorName.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0405_DE_GoblinWarriorName.d
@@ -2,10 +2,5 @@
  * #405 Spelling - Goblin Warrior (DE)
  */
 func int G1CP_0405_DE_GoblinWarriorName() {
-    return G1CP_Tpl_ChangeNpcStrVar(
-        "BlackGobboWarrior",
-        "name",
-        "Goblin Krieger",
-        "Goblinkrieger"
-    );
+    return G1CP_Tpl_ChangeNpcStrVar("BlackGobboWarrior", "name", "Goblin Krieger", "Goblinkrieger");
 };
