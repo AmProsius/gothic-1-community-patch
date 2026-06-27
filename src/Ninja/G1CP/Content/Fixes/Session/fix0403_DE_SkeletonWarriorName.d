4023509e6e62295e696c54294acc7d7f519ffce1
Botwyn <98276391+botwyn[bot]@users.noreply.github.com>
2026-06-27 17:43:58 +0000

    Initialize work on #403

---
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0403_DE_SkeletonWarriorName.d
@@ -0,0 +1,11 @@
+/*
+ * #403 Spelling - Skeleton Warrior 2 (DE)
+ */
+func int G1CP_0403_DE_SkeletonWarriorName() {
+    return G1CP_Tpl_ChangeNpcStrVar(
+        "### TODO NPC instance name ###",
+        "name",
+        "### TODO Original name ###",
+        "### TODO Correct name ###"
+    );
+};
---
+++ src/Ninja/G1CP/Content/Tests/test0403.d
@@ -0,0 +1,8 @@
+/*
+ * #403 Spelling - Skeleton Warrior 2 (DE)
+ */
+func void G1CP_Test_0403() {
+    G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
+    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("### TODO NPC instance ###");
+    G1CP_Testsuite_AssertS(npc.name, "### TODO Correct name ###");
+};
