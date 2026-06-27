36787aa430f0235bd4b5b165e516a8807eba8833
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 19:46:58 +0200

    Add todo values

--- src/Ninja/G1CP/Content/Fixes/Session/fix0404_DE_SkeletonWarriorName.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0404_DE_SkeletonWarriorName.d
@@ -2,10 +2,5 @@
  * #404 Spelling - Skeleton Warrior 3 (DE)
  */
 func int G1CP_0404_DE_SkeletonWarriorName() {
-    return G1CP_Tpl_ChangeNpcStrVar(
-        "### TODO NPC instance name ###",
-        "name",
-        "### TODO Original name ###",
-        "### TODO Correct name ###"
-    );
+    return G1CP_Tpl_ChangeNpcStrVar("SummonedByNPC_SkeletonWarrior", "name", "Skelett Krieger", "Skelettkrieger");
 };
--- src/Ninja/G1CP/Content/Tests/test0404.d
+++ src/Ninja/G1CP/Content/Tests/test0404.d
@@ -3,6 +3,6 @@
  */
 func void G1CP_Test_0404() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
-    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("### TODO NPC instance ###");
-    G1CP_Testsuite_AssertS(npc.name, "### TODO Correct name ###");
+    var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("SummonedByNPC_SkeletonWarrior");
+    G1CP_Testsuite_AssertS(npc.name, "Skelettkrieger");
 };
