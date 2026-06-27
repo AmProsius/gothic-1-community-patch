43562e65fdd07829714573f336eba940b3b2cc97
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 22:54:29 +0200

    Fix test

--- src/Ninja/G1CP/Content/Tests/test0439.d
+++ src/Ninja/G1CP/Content/Tests/test0439.d
@@ -1,9 +1,9 @@
 /*
  * #439 Spelling - Raw Meat (DE)
  */
-func int G1CP_Test_0439() {
+func void G1CP_Test_0439() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     var C_Item itm; itm = G1CP_Testsuite_CreateItem("ItFoMuttonRaw");
 
-    return G1CP_Testsuite_InspectItemString(itm, "name", "Rohes Fleisch");
+    G1CP_Testsuite_AssertS(itm.name, "Rohes Fleisch");
 };
