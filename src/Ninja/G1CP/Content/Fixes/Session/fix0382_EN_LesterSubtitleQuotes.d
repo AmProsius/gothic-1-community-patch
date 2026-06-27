f8663a1169ea4cd455a14e7680ae4eb7c11b4993
szapp <dev.szapp@gmail.com>
2026-06-27 20:35:05 +0200

    Implement subtile text with quotes

--- src/Ninja/G1CP/Content/Fixes/Session/fix0382_EN_LesterSubtitleQuotes.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0382_EN_LesterSubtitleQuotes.d
@@ -2,9 +2,13 @@
  * #382 Spelling - There are quotation marks in Lester's subtitles (EN)
  */
 func int G1CP_0382_EN_LesterSubtitleQuotes() {
+    // Create one character string with containing one double quote
+    const int DOUBLE_QUOTE = 34;
+    const string quote = " "; MEM_WriteByte(STR_toChar(quote), DOUBLE_QUOTE);
+    const string cleaned = "That's the smithy! Have a look around; I'll wait here for about an hour, then I'll leave.";
     return G1CP_ReplaceOuText(
-        "### TODO Output unit name ###",
-        "### TODO Original subtitles ###",
-        "### TODO Corrected subtitles ###"
+        "PC_Psionic_SMITH_Info_05_01",
+        ConcatStrings(ConcatStrings(quote, cleaned), quote),
+        cleaned
     );
 };
