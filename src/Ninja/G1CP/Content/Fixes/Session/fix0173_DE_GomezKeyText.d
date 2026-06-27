0667ab241040326e77c8beb017c9725f0513e1d9
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 17:04:02 +0200

    Apply suggestion from @szapp
    Co-authored-by: Sören Zapp <dev.szapp@gmail.com>

--- src/Ninja/G1CP/Content/Fixes/Session/fix0173_DE_GomezKeyText.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0173_DE_GomezKeyText.d
@@ -2,5 +2,5 @@
  * #173 Spelling - Gomez' Key: "Gomez' Truhen" (DE)
  */
 func int G1CP_0173_DE_GomezKeyText() {
-  return G1CP_Tpl_ChangeItemStrVar("ItKe_Gomez_01", "text[0]", "Öffnet Gomez Truhen", "Öffnet Gomez' Truhen.");
+    return G1CP_Tpl_ChangeItemStrVar("ItKe_Gomez_01", "text[0]", "Öffnet Gomez Truhen", "Öffnet Gomez' Truhen.");
 };
