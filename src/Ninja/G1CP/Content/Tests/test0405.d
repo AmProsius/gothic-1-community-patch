79321f2905e508cf74b60d0638b6c929f38c4db1
Fabian Bastwöste <ff@amprosius.de>
2026-06-27 17:38:34 +0200

    Fix test return value

--- src/Ninja/G1CP/Content/Tests/test0405.d
+++ src/Ninja/G1CP/Content/Tests/test0405.d
@@ -1,7 +1,7 @@
 /*
  * #405 Spelling - Goblin Warrior (DE)
  */
-func int G1CP_Test_0405() {
+func void G1CP_Test_0405() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("BlackGobboWarrior");
     G1CP_Testsuite_AssertS(npc.name, "Goblinkrieger");
--- templates/renameNpc/test.d
+++ templates/renameNpc/test.d
@@ -1,7 +1,7 @@
 /*
  * #{ISSUE_NUM} {LONGNAME}
  */
-func int G1CP_Test_{ISSUE_NUM_PAD}() {
+func void G1CP_Test_{ISSUE_NUM_PAD}() {
     {LANGCHECK}
     var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("### TODO NPC instance ###");
     G1CP_Testsuite_AssertS(npc.name, "### TODO Correct name ###");
