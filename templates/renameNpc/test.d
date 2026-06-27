e90830393a45b86376dd96152aaa9e812a766792
szapp <dev.szapp@gmail.com>
2026-06-27 14:04:02 +0200

    Improve test of renameNpc template

--- templates/renameNpc/test.d
+++ templates/renameNpc/test.d
@@ -4,6 +4,5 @@
 func int G1CP_Test_{ISSUE_NUM_PAD}() {
     {LANGCHECK}
     var C_Npc npc; npc = G1CP_Testsuite_CreateNpc("### TODO NPC instance ###");
-
-    return G1CP_Testsuite_InspectNpcString(npc, "name", "### TODO Correct name ###");
+    G1CP_Testsuite_AssertS(npc.name, "### TODO Correct name ###");
 };
