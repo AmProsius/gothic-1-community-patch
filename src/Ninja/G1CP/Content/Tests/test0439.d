676d70ae90f985b93c7ab40b662e8a9dbbbfee6b
Fabian Bastwöste <ff@amprosius.de>
2026-06-28 09:51:28 +0200

    Apply suggestions from code review
    Co-authored-by: Sören Zapp <github.zr737@slmail.me>

--- src/Ninja/G1CP/Content/Tests/test0439.d
+++ src/Ninja/G1CP/Content/Tests/test0439.d
@@ -4,6 +4,5 @@
 func void G1CP_Test_0439() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
     var C_Item itm; itm = G1CP_Testsuite_CreateItem("ItFoMuttonRaw");
-
     G1CP_Testsuite_AssertS(itm.name, "Rohes Fleisch");
 };
