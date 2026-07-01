24ad75b42a5b67539dd3f3e2dd300a461707e63d
Fabian Bastwöste <ff@amprosius.de>
2026-07-01 19:33:32 +0200

    Add check to fix

--- src/Ninja/G1CP/Content/Fixes/Session/fix0270_DE_OreBaronCellarKeyText.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0270_DE_OreBaronCellarKeyText.d
@@ -2,20 +2,43 @@
  * #270 Spelling - Key to the main door of the storage rooms in the cellar of the ore barons (DE)
  */
 func int G1CP_0270_DE_OreBaronCellarKeyText() {
-    const int count = -1;
+    const int count = 0;
 
-    count = G1CP_Tpl_ChangeItemStrVar(
+    var int itemId; itemId = G1CP_GetItemInstId("ItKe_Storage_01");
+    if (
+        STR_Compare(
+            G1CP_GetStringI(itemId, 0, "G1CP invalid string"),
+            "Öffnet Haupttür der Lagerräume "
+        ) == STR_EQUAL
+    ) {
+        count += 1;
+    };
+
+    if (
+        STR_Compare(
+            G1CP_GetStringI(itemId, 1, "G1CP invalid string"),
+            "im Keller der Erzbarone"
+        ) == STR_EQUAL
+    ) {
+        count += 1;
+    };
+
+    if (count <= 2) {
+        return FALSE;
+    };
+
+    G1CP_Tpl_ChangeItemStrVar(
         "ItKe_Storage_01",
         "text[0]",
         "Öffnet Haupttür der Lagerräume ",
         "Öffnet die Haupttür der Lager-"
     );
-    count += G1CP_Tpl_ChangeItemStrVar(
+    G1CP_Tpl_ChangeItemStrVar(
         "ItKe_Storage_01",
         "text[1]",
         "im Keller der Erzbarone",
         "räume im Keller der Erzbarone."
     );
 
-    return (count > -1);
+    return TRUE;
 };
