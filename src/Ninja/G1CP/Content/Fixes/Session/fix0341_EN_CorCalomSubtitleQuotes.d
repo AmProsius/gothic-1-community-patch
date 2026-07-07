2ae1ede657228c7a41ca9c66630bf6577537b716
szapp <dev.szapp@gmail.com>
2026-07-07 20:19:32 +0200

    Implement quotes into string
    Ref #341

--- src/Ninja/G1CP/Content/Fixes/Session/fix0341_EN_CorCalomSubtitleQuotes.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0341_EN_CorCalomSubtitleQuotes.d
@@ -2,9 +2,20 @@
  * #341 Spelling - There are quotation marks in the subtitles of the welcome dialog with Cor Calom (EN)
  */
 func int G1CP_0341_EN_CorCalomSubtitleQuotes() {
-    return G1CP_ReplaceOuText(
-        "### TODO Output unit name ###",
-        "### TODO Original subtitles ###",
-        "### TODO Corrected subtitles ###"
-    );
+    const int DOUBLE_QUOTE = 34;
+    const string original = "";
+    const string cleaned = "That was it? No welcome, no nothing?";
+    if (SB_New()) {
+        SBc(DOUBLE_QUOTE);
+        SB("That was it? No ");
+        SBc(DOUBLE_QUOTE); SBc(DOUBLE_QUOTE);
+        SB("welcome");
+        SBc(DOUBLE_QUOTE); SBc(DOUBLE_QUOTE);
+        SB(", no nothing?");
+        SBc(DOUBLE_QUOTE);
+        original = SB_ToString();
+        SB_Destroy();
+    };
+
+    return G1CP_ReplaceOuText("GUR_1201_CorKalom_JoinPSI_15_10", original, cleaned);
 };
--- src/Ninja/G1CP/Content/Tests/test0341.d
+++ src/Ninja/G1CP/Content/Tests/test0341.d
@@ -3,6 +3,6 @@
  */
 func int G1CP_Test_0341() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_EN);
-    var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("### TODO Output unit name ###");
-    G1CP_Testsuite_AssertS(G1CP_GetOuTextP(ouPtr), "### TODO Corrected subtitles ###");
+    var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("GUR_1201_CorKalom_JoinPSI_15_10");
+    G1CP_Testsuite_AssertS(G1CP_GetOuTextP(ouPtr), "That was it? No welcome, no nothing?");
 };
