bd05f95b0186eed3cdabf42848e118ce87d3f3cb
Fabian Bastwöste <ff@amprosius.de>
2026-07-07 18:34:39 +0200

    Add todo values

--- src/Ninja/G1CP/Content/Fixes/Session/fix0268_DE_LaboratoryKeyText.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0268_DE_LaboratoryKeyText.d
@@ -2,10 +2,21 @@
  * #268 Spelling - Text of key of the laboratory chest (DE)
  */
 func int G1CP_0268_DE_LaboratoryKeyText() {
-    return G1CP_Tpl_ChangeItemStrVar(
-        "### TODO Item instance name ###",
-        "text[### TODO Text array index ###]",
-        "### TODO Original text ###",
-        "### TODO Correct text ###"
+    var int applied1;
+    var int applied2;
+
+    applied1 = G1CP_Tpl_ChangeItemStrVar(
+        "ITKE_PSI_KALOM_01",
+        "text[0]",
+        "öffnet die Truhe im Alchemielabor",
+        "Öffnet die Truhe im Alchemielabor"
     );
+    applied2 = G1CP_Tpl_ChangeItemStrVar(
+        "ITKE_PSI_KALOM_01",
+        "text[1]",
+        "des Sumpflagers",
+        "des Sumpflagers."
+    );
+
+    return (applied1 || applied2);
 };
--- src/Ninja/G1CP/Content/Tests/test0268.d
+++ src/Ninja/G1CP/Content/Tests/test0268.d
@@ -3,7 +3,9 @@
  */
 func int G1CP_Test_0268() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_DE);
-    var C_Item itm; itm = G1CP_Testsuite_CreateItem("### TODO Item instance name ###");
+    var C_Item itm; itm = G1CP_Testsuite_CreateItem("ITKE_PSI_KALOM_01");
+    var string itm_text_1; itm_text_1 = MEM_ReadStatStringArr(itm.text, 1); // itm.text[1]
 
-    G1CP_Testsuite_AssertS(itm.text, "### TODO Expected string ###");
+    G1CP_Testsuite_AssertS(itm.text, "Öffnet die Truhe im Alchemielabor");
+    G1CP_Testsuite_AssertS(itm_text_1, "des Sumpflagers.");
 };
