79786a03bdd98d7089f51607f44482d4685b3085
Fabian Bastwöste <ff@amprosius.de>
2026-07-14 18:24:12 +0200

    Add todo values

--- src/Ninja/G1CP/Content/Fixes/Session/fix0344_EN_IanSubtitleQuotes.d
+++ src/Ninja/G1CP/Content/Fixes/Session/fix0344_EN_IanSubtitleQuotes.d
@@ -2,9 +2,22 @@
  * #344 Spelling - There are quotation marks in Ian's subtitles (EN)
  */
 func int G1CP_0344_EN_IanSubtitleQuotes() {
+    // Create one character string with containing one double quote
+    const int DOUBLE_QUOTE = 34;
+    const string quote = " "; MEM_WriteByte(STR_toChar(quote), DOUBLE_QUOTE);
+    const string cleaned = ConcatStrings(
+        ConcatStrings(
+            ConcatStrings(
+                ConcatStrings("Just tell him ", quote),
+                "Everything's gonna be fine"
+            ),
+            quote
+        ),
+        ". Then he'll know that I've given you permission."
+    );
     return G1CP_ReplaceOuText(
-        "### TODO Output unit name ###",
-        "### TODO Original subtitles ###",
-        "### TODO Corrected subtitles ###"
+        "STT_301_IAN_GEAR_SUC_Info_13_04",
+        ConcatStrings(ConcatStrings(quote, cleaned), quote),
+        cleaned
     );
 };
--- src/Ninja/G1CP/Content/Tests/test0344.d
+++ src/Ninja/G1CP/Content/Tests/test0344.d
@@ -3,6 +3,19 @@
  */
 func int G1CP_Test_0344() {
     G1CP_Testsuite_CheckLang(G1CP_Lang_EN);
-    var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("### TODO Output unit name ###");
-    G1CP_Testsuite_AssertS(G1CP_GetOuTextP(ouPtr), "### TODO Corrected subtitles ###");
+    var int ouPtr; ouPtr = G1CP_Testsuite_CheckOu("STT_301_IAN_GEAR_SUC_Info_13_04");
+    // Create one character string with containing one double quote
+    const int DOUBLE_QUOTE = 34;
+    const string quote = " "; MEM_WriteByte(STR_toChar(quote), DOUBLE_QUOTE);
+    const string cleaned = ConcatStrings(
+        ConcatStrings(
+            ConcatStrings(
+                ConcatStrings("Just tell him ", quote),
+                "Everything's gonna be fine"
+            ),
+            quote
+        ),
+        ". Then he'll know that I've given you permission."
+    );
+    G1CP_Testsuite_AssertS(G1CP_GetOuTextP(ouPtr), cleaned);
 };
