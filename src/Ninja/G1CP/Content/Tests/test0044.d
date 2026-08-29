3347dbe207bbbda6ca224be58f92a30e45775c95
szapp <dev.szapp@gmail.com>
2026-08-29 14:19:59 +0200

    Refactor changeItemInstanceVariableInt
    Ref #410 #454

--- src/Ninja/G1CP/Content/Tests/test0044.d
+++ src/Ninja/G1CP/Content/Tests/test0044.d
@@ -1,12 +1,7 @@
 /*
  * #44 Bugmeat in the wrong inventory category
- *
- * The mainflag (item category) of the item "ItAt_Meatbug_01" is inspected programmatically.
- *
- * Expected behavior: The item will have the correct mainflag.
  */
 func void G1CP_Test_0044() {
-    var C_Item itm; itm = G1CP_Testsuite_CreateItem("ItAt_Meatbug_01");
     const int ITEM_KAT_FOOD = 1 << 5;
-    G1CP_Testsuite_Assert(itm.mainflag, ITEM_KAT_FOOD);
+    G1CP_Tpl_TestItemIntVar("ItAt_Meatbug_01", "mainflag", ITEM_KAT_FOOD);
 };
